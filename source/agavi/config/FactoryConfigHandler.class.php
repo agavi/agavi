a89beae9816ea6ed0066e8895660fac5d1382045
Bob Zoller <bob@zoller.us>
2005-05-04 03:46:40 +0000

    implement zimba's MultipleControllerFactory from Mojavi forums http://forum.mojavi.org/index.php?showtopic=958&hl=xmlrpc closes #1

--- source/agavi/config/FactoryConfigHandler.class.php
+++ source/agavi/config/FactoryConfigHandler.class.php
@@ -45,157 +45,137 @@ class FactoryConfigHandler extends IniConfigHandler
 	 */
 	public function & execute ($config)
 	{
-
-		// available list of factories
-		$factories = array('request', 'storage', 'user', 'security_filter');
-
+  	// available list of controllers
+		$required_controllers = array('Controller');
+  
+  	// available list of factories
+  	$required_factories = array('request', 'storage', 'user', 'security_filter');
+  
 		// set our required categories list and initialize our handler
-		$categories = array('required_categories' => $factories);
-
+		$categories = array('required_categories' => $required_controllers);
 		$this->initialize($categories);
-
+  
 		// parse the ini
 		$ini = $this->parseIni($config);
-
+  
+  	// Revers the order of the controllers
+  	$ini = array_reverse($ini, true);
+  
 		// init our data and includes arrays
-		$includes  = array();
-		$inits     = array();
-		$instances = array();
-
-		// let's do our fancy work
-		foreach ($factories as $factory)
-		{
-
-			// see if the factory exists
-			if (!isset($ini[$factory]))
-			{
-
-				// factory hasn't been registered
-				$error = 'Configuration file "%s" must register "%s" factory';
-				$error = sprintf($config, $factory);
-
-				throw new ParseException($error);
-
-			}
-
-			// get factory keys
-			$keys = $ini[$factory];
-
-			if (!isset($keys['class']))
-			{
-
-				// missing class key
-				$error = 'Configuration file "%s" specifies category ' .
-						 '"%s" with missing class key';
-				$error = sprintf($error, $config, $factory);
-
-				throw new ParseException($error);
-
-			}
-
-			$class = $keys['class'];
-
-			if (isset($keys['file']))
-			{
-
-				// we have a file to include
-				$file =& $keys['file'];
-				$file =  $this->replaceConstants($file);
-				$file =  $this->replacePath($file);
-
-				if (!is_readable($file))
-				{
-
-				    // factory file doesn't exist
-				    $error = 'Configuration file "%s" specifies class ' .
-						     '"%s" with nonexistent or unreadablefile ' .
-						     '"%s"';
-				    $error = sprintf($error, $config, $class, $file);
-
-				    throw new ParseException($error);
-
+  	$controllers = array();
+  
+  	// check that every controller has the right paramers
+  	foreach($ini as $controllerName => $factories) {
+   		// init our data and includes arrays
+	  	$includes  = array();
+	  	$inits     = array();
+	  	$instances = array();
+  
+   		// Build all classes  
+   		foreach($required_factories as $factory) {
+				if (!array_key_exists($factory, $factories)) {
+	 				$error = 'Configuration file "%s" is missing "%s" key in "%s" category';
+					$error = sprintf($error, $config, $factory, $controllerName);
+					throw new ParseException($error);
+				}
+	
+				// Get class name
+				$class = $factories[$factory];
+	
+				// parse parameters
+				$parameters = ParameterParser::parse($factories, $factory .'.param');
+	
+				// append new data
+				switch ($factory) {
+					case 'request':
+	
+						// append instance creation
+						$tmp = "\t\$this->request = " .  "Request::newInstance('%s');";
+						$instances[] = sprintf($tmp, $class);
+	
+						// append instance initialization
+						$tmp = "\t\$this->request->initialize(\$this->context, " .  "%s);";
+						$inits[] = sprintf($tmp, $parameters);
+	
+						break;
+					case 'security_filter':
+	
+						// append creation/initialization in one swipe
+						$tmp = "\n\tif (MO_USE_SECURITY)\n\t{\n" .
+						       "\t\t\$this->securityFilter = " .
+						       "SecurityFilter::newInstance('%s');\n" .
+						       "\t\t\$this->securityFilter->initialize(" .
+						       "\$this->context, %s);\n\t}\n";
+						$inits[] = sprintf($tmp, $class, $parameters);
+	
+						break;
+					case 'storage':
+	
+						// append instance creation
+						$tmp = "\t\$this->storage = " .
+						       "Storage::newInstance('%s');";
+						$instances[] = sprintf($tmp, $class);
+	
+						// append instance initialization
+						$tmp = "\t\$this->storage->initialize(\$this->context, " . "%s);";
+						$inits[] = sprintf($tmp, $parameters);
+	
+						break;
+					case 'user':
+	
+						// append instance creation
+						$tmp = "\t\$this->user = User::newInstance('%s');";
+						$instances[] = sprintf($tmp, $class);
+	
+						// append instance initialization
+						$tmp = "\t\$this->user->initialize(\$this->context, %s);";
+						$inits[] = sprintf($tmp, $parameters);
+						break;
+					default:
+					 continue;
 				}
 
-				// append our data
-				$tmp        = "require_once('%s');";
-				$includes[] = sprintf($tmp, $file);
-
+				if (isset($factories[$factory.'.file'])) {
+					// we have a file to include
+					$file =& $factories[$factory.'.file'];
+					$file =  $this->replaceConstants($file);
+					$file =  $this->replacePath($file);
+	
+					if (!is_readable($file)) {
+		
+						// factory file doesn't exist
+						$error = 'Configuration file "%s" specifies class ' .
+						         '"%s" with nonexistent or unreadablefile ' .
+						         '"%s"';
+						$error = sprintf($error, $config, $class, $file);
+	
+						throw new ParseException($error);
+		
+					}
+		
+					// append our data
+					$tmp        = "\trequire_once('%s');";
+					$includes[] = sprintf($tmp, $file);
+		
+				}
 			}
-
-			// parse parameters
-			$parameters = ParameterParser::parse($keys);
-
-			// append new data
-			switch ($factory)
-			{
-
-				case 'request':
-
-				    // append instance creation
-				    $tmp         = "\$this->request = " .
-						           "Request::newInstance('%s');";
-				    $instances[] = sprintf($tmp, $class);
-
-				    // append instance initialization
-				    $tmp     = "\$this->request->initialize(\$this->context, " .
-						       "%s);";
-				    $inits[] = sprintf($tmp, $parameters);
-
-				    break;
-
-				case 'security_filter':
-
-				    // append creation/initialization in one swipe
-				    $tmp     = "\nif (MO_USE_SECURITY)\n{\n" .
-						       "\t\$this->securityFilter = " .
-						       "SecurityFilter::newInstance('%s');\n" .
-						       "\t\$this->securityFilter->initialize(" .
-						       "\$this->context, %s);\n}\n";
-				    $inits[] = sprintf($tmp, $class, $parameters);
-
-				    break;
-
-				case 'storage':
-
-				    // append instance creation
-				    $tmp         = "\$this->storage = " .
-						           "Storage::newInstance('%s');";
-				    $instances[] = sprintf($tmp, $class);
-
-				    // append instance initialization
-				    $tmp     = "\$this->storage->initialize(\$this->context, " .
-						       "%s);";
-				    $inits[] = sprintf($tmp, $parameters);
-
-				    break;
-
-				case 'user':
-
-				    // append instance creation
-				    $tmp         = "\$this->user = User::newInstance('%s');";
-				    $instances[] = sprintf($tmp, $class);
-
-				    // append instance initialization
-				    $tmp     = "\$this->user->initialize(\$this->context, %s);";
-				    $inits[] = sprintf($tmp, $parameters);
-
-			}
-
+  	
+			// context creation
+			$context = "\t\$this->context = new Context(%s, %s, %s, %s, %s);";
+			$context = sprintf($context, '$this', '$this->request', '$this->user', '$this->storage',
+			'$this->databaseManager');
+		
+			$tmp = "if (\$this instanceof $controllerName)\n{\n%s\n%s\n%s\n%s\n\treturn;\n}";
+			$controllers[] = sprintf($tmp, implode("\n", $includes),
+			implode("\n", $instances), $context, implode("\n", $inits));
 		}
-
-		// context creation
-		$context = "\$this->context = new Context(%s, %s, %s, %s, %s);";
-		$context = sprintf($context, '$this', '$this->request',
-						   '$this->user', '$this->storage',
-						   '$this->databaseManager');
-
+	
 		// compile data
 		$retval = "<?php\n" .
-				  "// auth-generated by FactoryConfigHandler\n" .
-				  "// date: %s\n%s\n%s\n%s\n%s\n?>";
+		"// auth-generated by FactoryConfigHandler\n" .
+		"// date: %s\n%s\n?>";
 		$retval = sprintf($retval, date('m/d/Y H:i:s'),
-						  implode("\n", $includes), implode("\n", $instances),
-						  $context, implode("\n", $inits));
+		implode("\n", $controllers));
 
 		return $retval;
 
