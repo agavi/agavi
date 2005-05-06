86e5ed09131bfa8ef962fb61d096619282ca09e7
Bob Zoller <bob@zoller.us>
2005-05-06 07:23:25 +0000

    Initial working version of unit testing with PHPUnit2. All tests must be run using phing (test target).

---
+++ tests/core/AgaviObjectTest.php
@@ -0,0 +1,25 @@
+<?php
+require_once('tests/include.php');
+require_once('core/AgaviObject.class.php');
+
+class SampleAgaviObject extends AgaviObject
+{
+	private $_something;
+	public $_somethingelse;
+	public function __construct()
+	{
+		$this->_something = 'a value';
+		$this->_somethingelse = array('another', 'value');
+	}
+}
+
+class AgaviObjectTest extends PHPUnit2_Framework_TestCase
+{
+	public function testtoString()
+	{
+		$ao = new SampleAgaviObject();
+		self::assertEquals($ao->toString(), '_something: a value, _somethingelse: Array');
+	}
+}
+
+?>
