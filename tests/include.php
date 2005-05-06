46d8a3fb9443dd6820303c86fac2fbec4e5781d4
Bob Zoller <bob@zoller.us>
2005-05-06 16:50:24 +0000

    make tests working again after the directory restructuring

--- tests/include.php
+++ tests/include.php
@@ -1,4 +1,4 @@
 <?php
 require_once('PHPUnit2/Framework/TestCase.php');
-set_include_path(get_include_path().':'.dirname(dirname(__file__)).'/source/agavi');
+set_include_path(get_include_path().':'.dirname(dirname(__file__)).'/src');
 ?>
