74e61451cf4855b232a80b2b0cffca0f6af95c7a
Mike Vincent <mike@vincent.ws>
2005-06-01 14:46:17 +0000

    added tests for Validator base class

--- tests/validator/ValidatorTest.php
+++ tests/validator/ValidatorTest.php
@@ -10,19 +10,45 @@ class SampleValidator extends Validator
 
 class ValidatorTest extends UnitTestCase
 {
+	private $_validator = null,
+					$_contreoller = null,
+					$_context = null;
+					
 	public function setUp()
 	{
-		$this->_v = new SampleValidator();
+		$this->_controller = new MockController($this);
+		$this->_controller->dispatch();
+		$this->_context = $this->_controller->getContext();
+		
+		$this->_validator = new SampleValidator();
+	}
+
+	public function tearDown()
+	{
+		$this->_validator = null;
+		$this->_controller = null;
+		$this->_context->cleanSlate();
+		$this->_context = null;
 	}
 
 	public function testinitialize()
 	{
-		$this->fail('Incomplete Test');
+		$this->assertNull($this->_validator->getContext());
+		$this->assertTrue($this->_validator->initialize($this->_context));
+		$this->assertTrue($this->_validator->getContext() instanceof MockContext );
+	}
+
+	public function testinitializeWithParameters()
+	{
+		$this->assertTrue($this->_validator->initialize($this->_context, array('foo' => 'bar')));
+		$this->assertIdentical('bar', $this->_validator->getParameter('foo'));
 	}
 
 	public function testgetContext()
 	{
-		$this->fail('Incomplete Test');
+		$this->_validator->initialize($this->_context);
+		$c = $this->_validator->getContext();
+		$this->assertReference($this->_context, $c);
 	}
 }
 
