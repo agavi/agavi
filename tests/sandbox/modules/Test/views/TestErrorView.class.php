50451e34eb5c5fa7666212bb27bfc671f5404460
Mike Vincent <mike@vincent.ws>
2005-06-01 02:41:31 +0000

    forgot to add the 'universal' testing sandbox

---
+++ tests/sandbox/modules/Test/views/TestErrorView.class.php
@@ -0,0 +1,13 @@
+<?php
+require_once('view/View.class.php');
+require_once('view/PHPView.class.php');
+
+class TestErrorView extends PHPView
+{
+	public function execute()
+	{
+		return null;
+	}
+}
+
+?>
