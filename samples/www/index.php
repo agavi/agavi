e4a69a82f34e839fc5ff37af886cebed7aec42d5
Mike Vincent <mike@vincent.ws>
2005-09-18 04:00:26 +0000

    Corrected index.php sample for new buiz..

--- samples/www/index.php
+++ samples/www/index.php
@@ -11,16 +11,7 @@ require_once('INSERT PATH TO "webapp/config.php" HERE');
 require_once('INSERT PATH TO "agavi/agavi.php" HERE');
 
 // +---------------------------------------------------------------------------+
-// | Create our controller. For this file we're going to use a front           |
-// | controller pattern. This pattern allows us to specify module and action   |
-// | GET/POST parameters and it automatically detects them and finds the       |
-// | expected action.                                                          |
+// | Call the controller's dispatch method on the default context              |
 // +---------------------------------------------------------------------------+
-$controller = Controller::newInstance('FrontWebController');
-
-// +---------------------------------------------------------------------------+
-// | Dispatch our request.                                                     |
-// +---------------------------------------------------------------------------+
-$controller->dispatch();
-
+Context::getInstance()->getController()->dispatch();
 ?>
