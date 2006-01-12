31412aa73027cd0b4b6b9f45f75ef4d37cda3a3b
David Zülke <david.zuelke@bitextender.com>
2006-01-12 05:55:52 +0000

    Backing out changes from 3bdbcb91edc7eedbfd628717c40c2dbe4efce845 since it broke agavi.org :)

---
+++ src/logging/MailAppender.class.php
@@ -0,0 +1,33 @@
+<?php
+
+// +---------------------------------------------------------------------------+
+// | This file is part of the Agavi package.                                   |
+// | Copyright (c) 2003-2006 the Agavi Project.                                |
+// |                                                                           |
+// | For the full copyright and license information, please view the LICENSE   |
+// | file that was distributed with this source code. You can also view the    |
+// | LICENSE file online at http://www.agavi.org/LICENSE.txt                   |
+// |   vi: set noexpandtab:                                                    |
+// |   Local Variables:                                                        |
+// |   indent-tabs-mode: t                                                     |
+// |   End:                                                                    |
+// +---------------------------------------------------------------------------+
+
+/**
+ * MailAppender appends Message objects to an email.
+ *
+ * @package    agavi
+ * @subpackage logging
+ *
+ * @author     Bob Zoller <bob@agavi.org>
+ * @copyright  (c) Authors
+ * @since      0.10.0
+ *
+ * @version    $Id$
+ */
+class MailAppender extends Appender
+{
+
+}
+
+?>
