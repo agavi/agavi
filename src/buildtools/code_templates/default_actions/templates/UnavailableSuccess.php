d4b20b41caceae1eb728908dd306f77bb55d5732
David Zülke <david.zuelke@bitextender.com>
2006-07-23 17:39:57 +0000

    added default action templates, your turn, Veikko :)

---
+++ src/buildtools/code_templates/default_actions/templates/UnavailableSuccess.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>Application Unavailable</title>
+	</head>
+	<body>
+		<h1>Application Unavailable</h1>
+		<p>This Application is currently not available. Please check back later.</p>
+	</body>
+</html>
