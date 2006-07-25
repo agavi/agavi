a91ed8bd6ccb1b59160285216877339581373667
David Zülke <david.zuelke@bitextender.com>
2006-07-25 16:10:36 +0000

    (hopefully) added support for Apache CGI to WebRouting

--- src/routing/AgaviWebRouting.class.php
+++ src/routing/AgaviWebRouting.class.php
@@ -62,6 +62,9 @@ class AgaviWebRouting extends AgaviRouting
 		if(isset($_SERVER['HTTP_X_REWRITE_URL'])) {
 			// Microsoft IIS with ISAPI_Rewrite
 			$ru = $_SERVER['HTTP_X_REWRITE_URL'];
+		} elseif(isset($_SERVER['ORIG_PATH_INFO']) && strpos($_SERVER['REQUEST_URI'], $_SERVER['ORIG_SCRIPT_NAME'] . $_SERVER['ORIG_PATH_INFO']) === 0) {
+			// Apache with CGI SAPI
+			$ru = $_SERVER['REQUEST_URI'];
 		} elseif(isset($_SERVER['ORIG_PATH_INFO'])) {
 			// Microsoft IIS
 			$ru = $_SERVER['ORIG_PATH_INFO'];
@@ -75,8 +78,8 @@ class AgaviWebRouting extends AgaviRouting
 		}
 		$ru = urldecode($ru);
 
-		if(isset($_SERVER['PATH_INFO'])) {
-			$this->prefix =  substr($ru, 0, -strlen($_SERVER['PATH_INFO']));
+		if(isset($_SERVER['PATH_INFO']) || isset($_SERVER['ORIG_PATH_INFO'])) {
+			$this->prefix =  substr($ru, 0, -strlen(isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : $_SERVER['ORIG_PATH_ÍNFO']));
 			$this->input = substr($ru, strlen($this->prefix));
 		} else {
 			$sn = $_SERVER['SCRIPT_NAME'];
