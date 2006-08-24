4c9b15653253dd1246812d671c69ac7418086dc9
David Zülke <david.zuelke@bitextender.com>
2006-08-24 10:16:53 +0000

    ConfigValueHolder: removed superfluous indent and made member declarations compatible with PHP guidelines

--- src/config/AgaviConfigValueHolder.class.php
+++ src/config/AgaviConfigValueHolder.class.php
@@ -25,305 +25,307 @@
  *
  * @version    $Id$
  */
-	class AgaviConfigValueHolder implements ArrayAccess, IteratorAggregate
+class AgaviConfigValueHolder implements ArrayAccess, IteratorAggregate
+{
+	/**
+	 * @var        string The name of this value.
+	 */
+	protected $_name = '';
+	/**
+	 * @var        array The attributes of this value.
+	 */
+	protected $_attributes = array();
+	/**
+	 * @var        array The child nodes of this value.
+	 */
+	protected $_childs = array();
+	/**
+	 * @var        string The value.
+	 */
+	protected $_value = null;
+
+	/**
+	 * Sets the name of this value.
+	 *
+	 * @param      string The name.
+	 *
+	 * @author     Dominik del Bondio <ddb@bitxtender.com>
+	 * @since      0.11.0
+	 */
+	public function setName($name)
 	{
-		/**
-		 * @var        string The name of this value.
-		 */
-		protected $__name = '';
-		/**
-		 * @var        array The attributes of this value.
-		 */
-		protected $__attributes = array();
-		/**
-		 * @var        array The child nodes of this value.
-		 */
-		protected $__childs = array();
-		/**
-		 * @var        string The value.
-		 */
-		protected $__value = null;
+		$this->_name = $name;
+	}
 
-		/**
-		 * Sets the name of this value.
-		 *
-		 * @param      string The name.
-		 *
-		 * @author     Dominik del Bondio <ddb@bitxtender.com>
-		 * @since      0.11.0
-		 */
-		public function setName($name)
-		{
-			$this->__name = $name;
-		}
+	/**
+	 * Returns the name of this value.
+	 *
+	 * @return     string The name.
+	 *
+	 * @author     Dominik del Bondio <ddb@bitxtender.com>
+	 * @since      0.11.0
+	 */
+	public function getName()
+	{
+		return $this->_name;
+	}
 
-		/**
-		 * Returns the name of this value.
-		 *
-		 * @return     string The name.
-		 *
-		 * @author     Dominik del Bondio <ddb@bitxtender.com>
-		 * @since      0.11.0
-		 */
-		public function getName()
-		{
-			return $this->__name;
-		}
-
-		/**
-		 * Adds a named children to this value. If a children with the same name
-		 * already exists the given value will be appended to the children.
-		 *
-		 * @param      string The name of the child.
-		 * @param      AgaviConfigValueHolder The child value.
-		 *
-		 * @author     Dominik del Bondio <ddb@bitxtender.com>
-		 * @since      0.11.0
-		 */
-		public function addChildren($name, $children)
-		{
-			if(!$this->hasChildren($name)) {
-				$this->$name = $children;
-				$this->__childs[$name] = $children;
-			} else {
-				$this->appendChildren($children);
-			}
-		}
-
-		/**
-		 * Adds a unnamed children to this value.
-		 *
-		 * @param      AgaviConfigValueHolder The child value.
-		 *
-		 * @author     Dominik del Bondio <ddb@bitxtender.com>
-		 * @since      0.11.0
-		 */
-		public function appendChildren($children)
-		{
-			$this->__childs[] = $children;
-		}
-
-		/**
-		 * Checks whether the value has children at all (no params) or whether a
-		 * child with the given name exists.
-		 *
-		 * @param      string The name of the child.
-		 *
-		 * @return     bool True if children exist, false if not.
-		 *
-		 * @author     Dominik del Bondio <ddb@bitxtender.com>
-		 * @since      0.11.0
-		 */
-		public function hasChildren($child = null)
-		{
-			if($child === null)
-				return count($this->__childs) > 0;
-
-			return isset($this->__childs[$child]);
-		}
-
-		/**
-		 * Returns the children of this value.
-		 *
-		 * @return     array An array with the childs of this value.
-		 *
-		 * @author     Dominik del Bondio <ddb@bitxtender.com>
-		 * @since      0.11.0
-		 */
-		public function getChildren()
-		{
-			return $this->__childs;
-		}
-
-		/**
-		 * Set an attribute.
-		 *
-		 * If an attribute with the name already exists the value will be
-		 * overridden.
-		 *
-		 * @param      string An attribute name.
-		 * @param      mixed  An attribute value.
-		 *
-		 * @author     Dominik del Bondio <ddb@bitxtender.com>
-		 * @since      0.11.0
-		 */
-		public function setAttribute($name, $value)
-		{
-			$this->__attributes[$name] = $value;
-		}
-
-		/**
-		 * Indicates whether or not an attribute exists.
-		 *
-		 * @param      string An attribute name.
-		 *
-		 * @return     bool true, if the attribute exists, otherwise false.
-		 *
-		 * @author     Dominik del Bondio <ddb@bitxtender.com>
-		 * @since      0.11.0
-		 */
-		public function hasAttribute($name)
-		{
-			return isset($this->__attributes[$name]);
-		}
-
-		/**
-		 * Retrieve an attribute.
-		 *
-		 * @param      string An attribute name.
-		 * @param      mixed  A default attribute value.
-		 *
-		 * @return     mixed An attribute value, if the attribute exists, otherwise
-		 *                   null or the given default.
-		 *
-		 * @author     Dominik del Bondio <ddb@bitxtender.com>
-		 * @since      0.11.0
-		 */
-		public function getAttribute($name, $default = null)
-		{
-			return isset($this->__attributes[$name]) ? $this->__attributes[$name] : $default;
-		}
-
-		/**
-		 * Retrieve all attributes.
-		 *
-		 * @return     array An associative array of attributes.
-		 *
-		 * @author     Dominik del Bondio <ddb@bitxtender.com>
-		 * @since      0.11.0
-		 */
-		public function getAttributes()
-		{
-			return $this->__attributes;
-		}
-
-		/**
-		 * Set the value of this value node.
-		 *
-		 * @param      string A value.
-		 *
-		 * @author     Dominik del Bondio <ddb@bitxtender.com>
-		 * @since      0.11.0
-		 */
-		public function setValue($value)
-		{
-			$this->__value = $value;
-		}
-
-		/**
-		 * Retrieves the value of this value node.
-		 *
-		 * @return     string The value of this node.
-		 *
-		 * @author     Dominik del Bondio <ddb@bitxtender.com>
-		 * @since      0.11.0
-		 */
-		public function getValue()
-		{
-			return $this->__value;
-		}
-
-		/**
-		 * Retrieves the info of this value node.
-		 *
-		 * @return     array An array containing the info for this node.
-		 *
-		 * @author     Dominik del Bondio <ddb@bitxtender.com>
-		 * @since      0.11.0
-		 */
-		public function getNode()
-		{
-			return array(
-				'name' => $this->__name,
-				'attributes' => $this->__attributes,
-				'children' => $this->__childs,
-				'value' => $this->__value,
-			);
-		}
-
-		/**
-		 * Determines if a named child exists. From ArrayAccess.
-		 *
-		 * @param      string Offset to check
-		 *
-		 * @return     bool Whether the offset exists.
-		 *
-		 * @author     Dominik del Bondio <ddb@bitxtender.com>
-		 * @since      0.11.0
-		 */
-		public function offsetExists($offset)
-		{
-			return isset($this->__childs[$offset]);
-		}
-
-		/**
-		 * Retrieves a named child. From ArrayAccess.
-		 *
-		 * @param      string Offset to retrieve
-		 *
-		 * @return     AgaviConfigValueHolder The child value.
-		 *
-		 * @author     Dominik del Bondio <ddb@bitxtender.com>
-		 * @since      0.11.0
-		 */
-		public function offsetGet($offset)
-		{
-			if(!isset($this->__childs[$offset]))
-				return null;
-			return $this->__childs[$offset];
-		}
-
-		/**
-		 * Inserts a named child. From ArrayAccess.
-		 *
-		 * @param      string Offset to modify
-		 * @param      AgaviConfigValueHolder The child value.
-		 *
-		 * @author     Dominik del Bondio <ddb@bitxtender.com>
-		 * @since      0.11.0
-		 */
-		public function offsetSet($offset, $value)
-		{
-			$this->__childs[$offset] = $value;
-		}
-
-		/**
-		 * Deletes a named child. From ArrayAccess.
-		 *
-		 * @return     string Offset to delete.
-		 *
-		 * @author     Dominik del Bondio <ddb@bitxtender.com>
-		 * @since      0.11.0
-		 */
-		public function offsetUnset($offset)
-		{
-			unset($this->__childs[$offset]);
-		}
-
-		/**
-		 * Returns an Iterator for the child nodes. From IteratorAggregate.
-		 *
-		 * @return     Iterator The iterator.
-		 *
-		 * @author     Dominik del Bondio <ddb@bitxtender.com>
-		 * @since      0.11.0
-		 */
-		public function getIterator()
-		{
-			return new ArrayIterator($this->getChildren());
-		}
-
-		/**
-		 * Retrieves the string representation of this value node. This is 
-		 * currently only the value of the node.
-		 *
-		 * @return     string The string representation.
-		 *
-		 * @author     Dominik del Bondio <ddb@bitxtender.com>
-		 * @since      0.11.0
-		 */
-		public function __toString()
-		{
-			return $this->__value;
+	/**
+	 * Adds a named children to this value. If a children with the same name
+	 * already exists the given value will be appended to the children.
+	 *
+	 * @param      string The name of the child.
+	 * @param      AgaviConfigValueHolder The child value.
+	 *
+	 * @author     Dominik del Bondio <ddb@bitxtender.com>
+	 * @since      0.11.0
+	 */
+	public function addChildren($name, $children)
+	{
+		if(!$this->hasChildren($name)) {
+			$this->$name = $children;
+			$this->_childs[$name] = $children;
+		} else {
+			$this->appendChildren($children);
 		}
 	}
+
+	/**
+	 * Adds a unnamed children to this value.
+	 *
+	 * @param      AgaviConfigValueHolder The child value.
+	 *
+	 * @author     Dominik del Bondio <ddb@bitxtender.com>
+	 * @since      0.11.0
+	 */
+	public function appendChildren($children)
+	{
+		$this->_childs[] = $children;
+	}
+
+	/**
+	 * Checks whether the value has children at all (no params) or whether a
+	 * child with the given name exists.
+	 *
+	 * @param      string The name of the child.
+	 *
+	 * @return     bool True if children exist, false if not.
+	 *
+	 * @author     Dominik del Bondio <ddb@bitxtender.com>
+	 * @since      0.11.0
+	 */
+	public function hasChildren($child = null)
+	{
+		if($child === null)
+			return count($this->_childs) > 0;
+
+		return isset($this->_childs[$child]);
+	}
+
+	/**
+	 * Returns the children of this value.
+	 *
+	 * @return     array An array with the childs of this value.
+	 *
+	 * @author     Dominik del Bondio <ddb@bitxtender.com>
+	 * @since      0.11.0
+	 */
+	public function getChildren()
+	{
+		return $this->_childs;
+	}
+
+	/**
+	 * Set an attribute.
+	 *
+	 * If an attribute with the name already exists the value will be
+	 * overridden.
+	 *
+	 * @param      string An attribute name.
+	 * @param      mixed  An attribute value.
+	 *
+	 * @author     Dominik del Bondio <ddb@bitxtender.com>
+	 * @since      0.11.0
+	 */
+	public function setAttribute($name, $value)
+	{
+		$this->_attributes[$name] = $value;
+	}
+
+	/**
+	 * Indicates whether or not an attribute exists.
+	 *
+	 * @param      string An attribute name.
+	 *
+	 * @return     bool true, if the attribute exists, otherwise false.
+	 *
+	 * @author     Dominik del Bondio <ddb@bitxtender.com>
+	 * @since      0.11.0
+	 */
+	public function hasAttribute($name)
+	{
+		return isset($this->_attributes[$name]);
+	}
+
+	/**
+	 * Retrieve an attribute.
+	 *
+	 * @param      string An attribute name.
+	 * @param      mixed  A default attribute value.
+	 *
+	 * @return     mixed An attribute value, if the attribute exists, otherwise
+	 *                   null or the given default.
+	 *
+	 * @author     Dominik del Bondio <ddb@bitxtender.com>
+	 * @since      0.11.0
+	 */
+	public function getAttribute($name, $default = null)
+	{
+		return isset($this->_attributes[$name]) ? $this->_attributes[$name] : $default;
+	}
+
+	/**
+	 * Retrieve all attributes.
+	 *
+	 * @return     array An associative array of attributes.
+	 *
+	 * @author     Dominik del Bondio <ddb@bitxtender.com>
+	 * @since      0.11.0
+	 */
+	public function getAttributes()
+	{
+		return $this->_attributes;
+	}
+
+	/**
+	 * Set the value of this value node.
+	 *
+	 * @param      string A value.
+	 *
+	 * @author     Dominik del Bondio <ddb@bitxtender.com>
+	 * @since      0.11.0
+	 */
+	public function setValue($value)
+	{
+		$this->_value = $value;
+	}
+
+	/**
+	 * Retrieves the value of this value node.
+	 *
+	 * @return     string The value of this node.
+	 *
+	 * @author     Dominik del Bondio <ddb@bitxtender.com>
+	 * @since      0.11.0
+	 */
+	public function getValue()
+	{
+		return $this->_value;
+	}
+
+	/**
+	 * Retrieves the info of this value node.
+	 *
+	 * @return     array An array containing the info for this node.
+	 *
+	 * @author     Dominik del Bondio <ddb@bitxtender.com>
+	 * @since      0.11.0
+	 */
+	public function getNode()
+	{
+		return array(
+			'name' => $this->_name,
+			'attributes' => $this->_attributes,
+			'children' => $this->_childs,
+			'value' => $this->_value,
+		);
+	}
+
+	/**
+	 * Determines if a named child exists. From ArrayAccess.
+	 *
+	 * @param      string Offset to check
+	 *
+	 * @return     bool Whether the offset exists.
+	 *
+	 * @author     Dominik del Bondio <ddb@bitxtender.com>
+	 * @since      0.11.0
+	 */
+	public function offsetExists($offset)
+	{
+		return isset($this->_childs[$offset]);
+	}
+
+	/**
+	 * Retrieves a named child. From ArrayAccess.
+	 *
+	 * @param      string Offset to retrieve
+	 *
+	 * @return     AgaviConfigValueHolder The child value.
+	 *
+	 * @author     Dominik del Bondio <ddb@bitxtender.com>
+	 * @since      0.11.0
+	 */
+	public function offsetGet($offset)
+	{
+		if(!isset($this->_childs[$offset]))
+			return null;
+		return $this->_childs[$offset];
+	}
+
+	/**
+	 * Inserts a named child. From ArrayAccess.
+	 *
+	 * @param      string Offset to modify
+	 * @param      AgaviConfigValueHolder The child value.
+	 *
+	 * @author     Dominik del Bondio <ddb@bitxtender.com>
+	 * @since      0.11.0
+	 */
+	public function offsetSet($offset, $value)
+	{
+		$this->_childs[$offset] = $value;
+	}
+
+	/**
+	 * Deletes a named child. From ArrayAccess.
+	 *
+	 * @return     string Offset to delete.
+	 *
+	 * @author     Dominik del Bondio <ddb@bitxtender.com>
+	 * @since      0.11.0
+	 */
+	public function offsetUnset($offset)
+	{
+		unset($this->_childs[$offset]);
+	}
+
+	/**
+	 * Returns an Iterator for the child nodes. From IteratorAggregate.
+	 *
+	 * @return     Iterator The iterator.
+	 *
+	 * @author     Dominik del Bondio <ddb@bitxtender.com>
+	 * @since      0.11.0
+	 */
+	public function getIterator()
+	{
+		return new ArrayIterator($this->getChildren());
+	}
+
+	/**
+	 * Retrieves the string representation of this value node. This is 
+	 * currently only the value of the node.
+	 *
+	 * @return     string The string representation.
+	 *
+	 * @author     Dominik del Bondio <ddb@bitxtender.com>
+	 * @since      0.11.0
+	 */
+	public function __toString()
+	{
+		return $this->_value;
+	}
+}
+
+?>
