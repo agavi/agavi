77d2fa87ad70ad5fc78a8b5a6645f151278c4ea4
David Zülke <david.zuelke@bitextender.com>
2006-10-01 22:14:39 +0000

    oops

--- samples/pub/index.php
+++ samples/pub/index.php
@@ -3,7 +3,7 @@
 // +---------------------------------------------------------------------------+
 // | An absolute filesystem path to the agavi/agavi.php script.                |
 // +---------------------------------------------------------------------------+
-require('../../src/agavi.php');
+require('INSERT PATH TO "agavi/agavi.php" HERE');
 
 // +---------------------------------------------------------------------------+
 // | An absolute filesystem path to our app/config.php script.                 |
