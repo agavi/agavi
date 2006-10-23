0b147bdb960ee9dcb7bc5f19c87818429202f5e7
Dominik del Bondio <dominik.del.bondio@bitextender.com>
2006-10-23 15:29:15 +0000

    phpdoc fixes

--- src/date/AgaviDateDefinitions.class.php
+++ src/date/AgaviDateDefinitions.class.php
@@ -20,7 +20,7 @@
  * @subpackage date
  *
  * @author     Dominik del Bondio <ddb@bitxtender.com>
- * @author     The ICU Project <http://icu.sourceforge.net>
+ * @author     The ICU Project ({@link http://icu.sourceforge.net})
  * @copyright  (c) Authors
  * @since      0.11.0
  *
@@ -29,8 +29,10 @@
 final class AgaviDateDefinitions
 {
 	/** 
-	 * Field number indicating the era, e.g., AD or BC in the Gregorian (Julian)
-	 * calendar. This is a calendar-specific value.
+	 * Field number indicating the era, e.g., {@link AgaviGregorianCalendar::AD} 
+	 * or {@link AgaviGregorianCalendar::BC} in the Gregorian (Julian) calendar.
+	 * 
+	 * This is a calendar-specific value.
 	 * 
 	 * @since      0.11.0
 	 */
@@ -44,61 +46,63 @@ final class AgaviDateDefinitions
 	const YEAR                    = 1;
 
 	/**
-	 * Field number indicating the month. This is a calendar-specific value. 
-	 * The first month of the year is
-	 * <code>JANUARY</code>; the last depends on the number of months in a year.
+	 * Field number indicating the month. 
 	 * 
-	 * @see #JANUARY
-	 * @see #FEBRUARY
-	 * @see #MARCH
-	 * @see #APRIL
-	 * @see #MAY
-	 * @see #JUNE
-	 * @see #JULY
-	 * @see #AUGUST
-	 * @see #SEPTEMBER
-	 * @see #OCTOBER
-	 * @see #NOVEMBER
-	 * @see #DECEMBER
-	 * @see #UNDECIMBER
+	 * This is a calendar-specific value. The first month of the year is 
+	 * {@link JANUARY}; the last depends on the number of months in a year.
+	 * 
+	 * @see        JANUARY
+	 * @see        FEBRUARY
+	 * @see        MARCH
+	 * @see        APRIL
+	 * @see        MAY
+	 * @see        JUNE
+	 * @see        JULY
+	 * @see        AUGUST
+	 * @see        SEPTEMBER
+	 * @see        OCTOBER
+	 * @see        NOVEMBER
+	 * @see        DECEMBER
+	 * @see        UNDECIMBER
 	 * 
 	 * @since      0.11.0
 	 */
 	const MONTH                   = 2;
 
 	/**
-	 * Field number indicating the
-	 * week number within the current year.  The first week of the year, as
-	 * defined by <code>FIRST_DAY_OF_WEEK</code> and 
-	 * <code>MINIMAL_DAYS_IN_FIRST_WEEK</code> attributes, has value 1.
-	 * Subclasses define the value of <code>WEEK_OF_YEAR</code> for days 
-	 * before the first week of the year.
+	 * Field number indicating the week number within the current year.
+	 * 
+	 * The first week of the year, as defined by <kbd>FIRST_DAY_OF_WEEK</kbd> and 
+	 * <kbd>MINIMAL_DAYS_IN_FIRST_WEEK</kbd> attributes, has value 1. Subclasses 
+	 * define the value of <kbd>WEEK_OF_YEAR</kbd> for days  before the first week
+	 * of the year.
 	 * 
 	 * @since      0.11.0
 	 */
 	const WEEK_OF_YEAR            = 3;
 
  /**
-	 * Field number indicating the
-	 * week number within the current month.  The first week of the month, as
-	 * defined by <code>FIRST_DAY_OF_WEEK</code> and 
-	 * <code>MINIMAL_DAYS_IN_FIRST_WEEK</code> attributes, has value 1.
-	 * Subclasses define the value of <code>WEEK_OF_MONTH</code> for days before 
+	 * Field number indicating the week number within the current month.
+	 * 
+	 * The first week of the month, as defined by <kbd>FIRST_DAY_OF_WEEK</kbd> and
+	 * <kbd>MINIMAL_DAYS_IN_FIRST_WEEK</kbd> attributes, has value 1.
+	 * Subclasses define the value of <kbd>WEEK_OF_MONTH</kbd> for days before 
 	 * the first week of the month.
 	 * 
-	 * @see #getFirstDayOfWeek
-	 * @see #getMinimalDaysInFirstWeek
+	 * @see        getFirstDayOfWeek
+	 * @see        getMinimalDaysInFirstWeek
 	 * 
 	 * @since      0.11.0
 	 */
 	const WEEK_OF_MONTH           = 4;
 
  /**
-	 * Field number indicating the
-	 * day of the month. This is a synonym for <code>DAY_OF_MONTH</code>.
-	 * The first day of the month has value 1.
+	 * Field number indicating the day of the month.
 	 * 
-	 * @see #DAY_OF_MONTH
+	 * This is a synonym for {@link DAY_OF_MONTH}. The first day of the month 
+	 * has value 1.
+	 * 
+	 * @see        DAY_OF_MONTH
 	 * 
 	 * @since      0.11.0
 	 */
@@ -106,6 +110,7 @@ final class AgaviDateDefinitions
 
  /**
 	 * Field number indicating the day number within the current year.
+	 * 
 	 * The first day of the year has value 1.
 	 * 
 	 * @since      0.11.0
@@ -113,80 +118,83 @@ final class AgaviDateDefinitions
 	const DAY_OF_YEAR             = 6;
 
  /**
-	 * Field number indicating the day
-	 * of the week.  This field takes values <code>SUNDAY</code>,
-	 * <code>MONDAY</code>, <code>TUESDAY</code>, <code>WEDNESDAY</code>,
-	 * <code>THURSDAY</code>, <code>FRIDAY</code>, and <code>SATURDAY</code>.
+	 * Field number indicating the day of the week.
 	 * 
-	 * @see #SUNDAY
-	 * @see #MONDAY
-	 * @see #TUESDAY
-	 * @see #WEDNESDAY
-	 * @see #THURSDAY
-	 * @see #FRIDAY
-	 * @see #SATURDAY
+	 * This field takes values {@link SUNDAY}, {@link MONDAY}, {@link TUESDAY}, 
+	 * {@link WEDNESDAY}, {@link THURSDAY}, {@link FRIDAY}, and {@link SATURDAY}.
+	 * 
+	 * @see        SUNDAY
+	 * @see        MONDAY
+	 * @see        TUESDAY
+	 * @see        WEDNESDAY
+	 * @see        THURSDAY
+	 * @see        FRIDAY
+	 * @see        SATURDAY
 	 * 
 	 * @since      0.11.0
 	 */
 	const DAY_OF_WEEK             = 7;
 
  /**
-	 * Field number indicating the
-	 * ordinal number of the day of the week within the current month. Together
-	 * with the <code>DAY_OF_WEEK</code> field, this uniquely specifies a day
-	 * within a month.  Unlike <code>WEEK_OF_MONTH</code> and
-	 * <code>WEEK_OF_YEAR</code>, this field's value does <em>not</em> depend on
-	 * <code>getFirstDayOfWeek()</code> or
-	 * <code>getMinimalDaysInFirstWeek()</code>.  <code>DAY_OF_MONTH 1</code>
-	 * through <code>7</code> always correspond to <code>DAY_OF_WEEK_IN_MONTH
-	 * 1</code>; <code>8</code> through <code>15</code> correspond to
-	 * <code>DAY_OF_WEEK_IN_MONTH 2</code>, and so on.
-	 * <code>DAY_OF_WEEK_IN_MONTH 0</code> indicates the week before
-	 * <code>DAY_OF_WEEK_IN_MONTH 1</code>.  Negative values count back from the
-	 * end of the month, so the last Sunday of a month is specified as
-	 * <code>DAY_OF_WEEK = SUNDAY, DAY_OF_WEEK_IN_MONTH = -1</code>.  Because
-	 * negative values count backward they will usually be aligned differently
-	 * within the month than positive values.  For example, if a month has 31
-	 * days, <code>DAY_OF_WEEK_IN_MONTH -1</code> will overlap
-	 * <code>DAY_OF_WEEK_IN_MONTH 5</code> and the end of <code>4</code>.
+	 * Field number indicating the ordinal number of the day of the week within 
+	 * the current month.
 	 * 
-	 * @see #DAY_OF_WEEK
-	 * @see #WEEK_OF_MONTH
+	 * Together with the {@link DAY_OF_WEEK} field, this uniquely specifies a day
+	 * within a month.  Unlike {@link WEEK_OF_MONTH} and {@link WEEK_OF_YEAR},
+	 * this field's value does <i>not</i> depend on
+	 * {@link AgaviCalendar::getFirstDayOfWeek()} or
+	 * {@link AgaviCalendar::getMinimalDaysInFirstWeek()}. 
+	 * <kbd>DAY_OF_MONTH 1</kbd> through <kbd>7</kbd> always correspond to 
+	 * <kbd>{@link DAY_OF_WEEK_IN_MONTH} 1</kbd>; <kbd>8</kbd> through 
+	 * <kbd>15</kbd> correspond to <kbd>{@link DAY_OF_WEEK_IN_MONTH} 2</kbd>, 
+	 * and so on. <kbd>{@link DAY_OF_WEEK_IN_MONTH} 0</kbd> indicates the week 
+	 * before <kbd>{@link DAY_OF_WEEK_IN_MONTH} 1</kbd>. Negative values count 
+	 * back from the end of the month, so the last Sunday of a month is specified
+	 * as <kbd>{@link DAY_OF_WEEK} = {@link SUNDAY}, 
+	 * {@link DAY_OF_WEEK_IN_MONTH} = -1</kbd>. Because negative values count 
+	 * backward they will usually be aligned differently within the month than 
+	 * positive values.  For example, if a month has 31 days, 
+	 * <kbd>{@link DAY_OF_WEEK_IN_MONTH} -1</kbd> will overlap
+	 * <kbd>{@link DAY_OF_WEEK_IN_MONTH} 5</kbd> and the end of <kbd>4</kbd>.
+	 * 
+	 * @see        DAY_OF_WEEK
+	 * @see        WEEK_OF_MONTH
 	 * 
 	 * @since      0.11.0
 	 */
 	const DAY_OF_WEEK_IN_MONTH    = 8;
 
  /**
-	 * Field number indicating
-	 * whether the <code>HOUR</code> is before or after noon.
-	 * E.g., at 10:04:15.250 PM the <code>AM_PM</code> is <code>PM</code>.
+	 * Field number indicating  whether the {@link HOUR} is before or after noon.
 	 * 
-	 * @see #AM
-	 * @see #PM
-	 * @see #HOUR
+	 * E.g., at 10:04:15.250 PM the <kbd>AM_PM</kbd> is {@link PM}.
+	 * 
+	 * @see        AM
+	 * @see        PM
+	 * @see        HOUR
 	 * 
 	 * @since      0.11.0
 	 */
 	const AM_PM                   = 9;
 
 	/**
-	 * Field number indicating the
-	 * hour of the morning or afternoon. <code>HOUR</code> is used for the 12-hour
-	 * clock.
-	 * E.g., at 10:04:15.250 PM the <code>HOUR</code> is 10.
+	 * Field number indicating the hour of the morning or afternoon.
 	 * 
-	 * @see #AM_PM
-	 * @see #HOUR_OF_DAY
+	 * <kbd>HOUR</kbd> is used for the 12-hour clock.
+	 * E.g., at 10:04:15.250 PM the <kbd>HOUR</kbd> is 10.
+	 * 
+	 * @see        AM_PM
+	 * @see        HOUR_OF_DAY
 	 * 
 	 * @since      0.11.0
 	 */
 	const HOUR                    = 10;
 
 	/**
-	 * Field number indicating the
-	 * hour of the day. <code>HOUR_OF_DAY</code> is used for the 24-hour clock.
-	 * E.g., at 10:04:15.250 PM the <code>HOUR_OF_DAY</code> is 22.
+	 * Field number indicating the hour of the day.
+	 * 
+	 * <kbd>HOUR_OF_DAY</kbd> is used for the 24-hour clock.
+	 * E.g., at 10:04:15.250 PM the <kbd>HOUR_OF_DAY</kbd> is 22.
 	 * 
 	 * @see #HOUR
 	 * 
@@ -195,70 +203,70 @@ final class AgaviDateDefinitions
 	const HOUR_OF_DAY             = 11;
 
 	/**
-	 * Field number indicating the
-	 * minute within the hour.
-	 * E.g., at 10:04:15.250 PM the <code>MINUTE</code> is 4.
+	 * Field number indicating the  minute within the hour.
+	 * 
+	 * E.g., at 10:04:15.250 PM the <kbd>MINUTE</kbd> is 4.
 	 * 
 	 * @since      0.11.0
 	 */
 	const MINUTE                  = 12;
 
 	/**
-	 * Field number indicating the
-	 * second within the minute.
-	 * E.g., at 10:04:15.250 PM the <code>SECOND</code> is 15.
+	 * Field number indicating the second within the minute.
+	 * 
+	 * E.g., at 10:04:15.250 PM the <kbd>SECOND</kbd> is 15.
 	 * 
 	 * @since      0.11.0
 	 */
 	const SECOND                  = 13;
 
 	/**
-	 * Field number indicating the
-	 * millisecond within the second.
-	 * E.g., at 10:04:15.250 PM the <code>MILLISECOND</code> is 250.
+	 * Field number indicating the millisecond within the second.
+	 * 
+	 * E.g., at 10:04:15.250 PM the <kbd>MILLISECOND</kbd> is 250.
 	 * 
 	 * @since      0.11.0
 	 */
 	const MILLISECOND             = 14;
 
 	/**
-	 * Field number indicating the
-	 * raw offset from GMT in milliseconds.
+	 * Field number indicating the raw offset from GMT in milliseconds.
 	 * 
 	 * @since      0.11.0
 	 */
 	const ZONE_OFFSET             = 15;
 
 	/**
-	 * Field number indicating the
-	 * daylight savings offset in milliseconds.
+	 * Field number indicating the  daylight savings offset in milliseconds.
 	 * 
 	 * @since      0.11.0
 	 */
 	const DST_OFFSET              = 16;
 	
 	/**
-	 * Field number 
-	 * indicating the extended year corresponding to the
-	 * <code>WEEK_OF_YEAR</code> field.  This may be one greater or less
-	 * than the value of <code>EXTENDED_YEAR</code>.
+	 * Field number indicating the extended year corresponding to the
+	 * {@link WEEK_OF_YEAR} field.
+	 * 
+	 * This may be one greater or less than the value of {@link EXTENDED_YEAR}.
 	 * 
 	 * @since      0.11.0
 	 */
 	const YEAR_WOY                = 17;
 
 	/**
-	 * Field number 
-	 * indicating the localized day of week.  This will be a value from 1
-	 * to 7 inclusive, with 1 being the localized first day of the week.
+	 * Field number indicating the localized day of week.
+	 * 
+	 * This will be a value from 1 to 7 inclusive, with 1 being the localized 
+	 * first day of the week.
 	 * 
 	 * @since      0.11.0
 	 */
 	const DOW_LOCAL               = 18;
 	
 	/**
-	 * Year of this calendar system, encompassing all supra-year fields. For 
-	 * example,  in Gregorian/Julian calendars, positive Extended Year values 
+	 * Year of this calendar system, encompassing all supra-year fields.
+	 *
+	 * For  example, in Gregorian/Julian calendars, positive Extended Year values 
 	 * indicate years AD, 1 BC = 0 extended, 2 BC = -1 extended, and so on.
 	 * 
 	 * @since      0.11.0
@@ -266,12 +274,12 @@ final class AgaviDateDefinitions
 	const EXTENDED_YEAR           = 19;
  
 	/**
-	 * Field number 
-	 * indicating the modified Julian day number.  This is different from
-	 * the conventional Julian day number in two regards.  First, it
-	 * demarcates days at local zone midnight, rather than noon GMT.
+	 * Field number indicating the modified Julian day number.
+	 * 
+	 * This is different from the conventional Julian day number in two regards.
+	 * First, it demarcates days at local zone midnight, rather than noon GMT.
 	 * Second, it is a local number; that is, it depends on the local time
-	 * zone.  It can be thought of as a single number that encompasses all
+	 * zone. It can be thought of as a single number that encompasses all
 	 * the date-related fields.
 	 * 
 	 * @since      0.11.0
@@ -279,13 +287,14 @@ final class AgaviDateDefinitions
 	const JULIAN_DAY              = 20;
 
 	/**
-	 * Ranges from 0 to 23:59:59.999 (regardless of DST).  This field behaves 
-	 * <em>exactly</em>  like a composite of all time-related fields, not 
-	 * including the zone fields.  As such,  it also reflects discontinuities of 
-	 * those fields on DST transition days.  On a day of DST onset, it will jump 
-	 * forward.  On a day of DST cessation, it will jump  backward.  This reflects
-	 * the fact that it must be combined with the DST_OFFSET field to obtain a 
-	 * unique local time value.
+	 * Ranges from 0 to 23:59:59.999 (regardless of DST).
+	 * 
+	 * This field behaves <i>exactly</i> like a composite of all time-related 
+	 * fields, not  including the zone fields. As such, it also reflects 
+	 * discontinuities of  those fields on DST transition days. On a day of DST 
+	 * onset, it will jump  forward. On a day of DST cessation, it will jump
+	 * backward.  This reflects the fact that it must be combined with the 
+	 * {@link DST_OFFSET} field to obtain a unique local time value.
 	 * 
 	 * @since      0.11.0
 	 */
@@ -299,12 +308,11 @@ final class AgaviDateDefinitions
 	const FIELD_COUNT             = 22;
 
 	/**
-	 * Field number indicating the
-	 * day of the month. This is a synonym for <code>DATE</code>.
-	 * The first day of the month has value 1.
-	 * Synonym for DATE
+	 * Field number indicating the day of the month.
 	 * 
-	 * @see #DATE
+	 * This is a synonym for {@link DATE}. The first day of the month has value 1.
+	 * 
+	 * @see        DATE
 	 * 
 	 * @since      0.11.0
 	 */
@@ -335,7 +343,7 @@ final class AgaviDateDefinitions
 	const NOVEMBER                = 10;
 	/** December */
 	const DECEMBER                = 11;
-	/** Value of the <code>MONTH</code> field indicating the
+	/** Value of the <kbd>MONTH</kbd> field indicating the
 		* thirteenth month of the year. Although the Gregorian calendar
 		* does not use this value, lunar calendars do.
 		*/
