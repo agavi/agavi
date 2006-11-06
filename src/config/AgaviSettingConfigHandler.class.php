25290904f7d9679609feb2d87954e5682fe835dc
David Zülke <david.zuelke@bitextender.com>
2006-11-06 23:01:53 +0000

    fixed SettingsConfigHandler assuming existance of a settings tag

--- src/config/AgaviSettingConfigHandler.class.php
+++ src/config/AgaviSettingConfigHandler.class.php
@@ -64,9 +64,11 @@ class AgaviSettingConfigHandler extends AgaviConfigHandler
 				}
 			}
 
-			foreach($cfg->settings as $setting)
-			{
-				$data['core.' . $setting->getAttribute('name')] = $this->literalize($this->replaceConstants($setting->getValue()));
+			if(isset($cfg->settings)) {
+				foreach($cfg->settings as $setting)
+				{
+					$data['core.' . $setting->getAttribute('name')] = $this->literalize($this->replaceConstants($setting->getValue()));
+				}
 			}
 			
 			if($cfg->hasChildren('exception_templates')) {
