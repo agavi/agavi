d6896a0dbbb7c1f7ec841d5c8a2edca96ff4d5ad
Dominik del Bondio <dominik.del.bondio@bitextender.com>
2006-11-06 00:08:09 +0000

    Add some tests for the DecimalFormatter

--- tests2/AllTests/UtilTests.php
+++ tests2/AllTests/UtilTests.php
@@ -2,6 +2,7 @@
 require_once(dirname(__FILE__) . '/../util/AttributeHolderTest.php');
 require_once(dirname(__FILE__) . '/../util/ParameterHolderTest.php');
 require_once(dirname(__FILE__) . '/../util/ToolkitTest.php');
+require_once(dirname(__FILE__) . '/../util/DecimalFormatterTest.php');
 //require_once(dirname(__FILE__) . '/../util/PathTest.php');
 
 class UtilTests
@@ -13,6 +14,7 @@ class UtilTests
 		$suite->addTestSuite('AttributeHolderTest');
 		$suite->addTestSuite('ParameterHolderTest');
 		$suite->addTestSuite('ToolkitTest');
+		$suite->addTestSuite('DecimalFormatterTest');
 //		$suite->addTestSuite('PathTest');
 
 		return $suite;
