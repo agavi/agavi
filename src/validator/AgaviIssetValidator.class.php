f2c31236d786fa1ae05537e3a7ca750f77ba9f68
Dominik del Bondio <dominik.del.bondio@bitextender.com>
2007-01-19 04:05:24 +0000

    made it possible again to set isset validators to required=false

--- src/validator/AgaviIssetValidator.class.php
+++ src/validator/AgaviIssetValidator.class.php
@@ -31,23 +31,6 @@
 class AgaviIssetValidator extends AgaviValidator
 {
 	/**
-	 * We return true here no matter what because we will check for existance
-	 * ourself.
-	 *
-	 * @param      bool Whether an error should be thrown for each missing 
-	 *                  argument if this validator is required.
-	 *
-	 * @return     bool Whether the arguments are set.
-	 *
-	 * @author     Dominik del Bondio <ddb@bitxtender.com>
-	 * @since      0.11.0
-	 */
-	protected function checkAllArgumentsSet($throwError = true)
-	{
-		return true;
-	}
-
-	/**
 	 * Validates the input.
 	 * 
 	 * @return     bool The value is set.
