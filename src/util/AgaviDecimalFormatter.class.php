abe96ed68eca739cb0117edcb247ef252a6a509c
Dominik del Bondio <dominik.del.bondio@bitextender.com>
2007-01-30 23:51:16 +0000

    fixed #399

--- src/util/AgaviDecimalFormatter.class.php
+++ src/util/AgaviDecimalFormatter.class.php
@@ -643,6 +643,10 @@ class AgaviDecimalFormatter
 				$num = $num * pow(10, $match['exp']);
 			}
 
+			if(!empty($match['sign']) && '-' == $match['sign']) {
+				$num = $num * -1;
+			}
+
 			return $num;
 		} else {
 			if(strlen($string) > 0) {
