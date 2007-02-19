agavi/agavi

commit ec12c880c4ef722d1befc6e88adf3de1d6edac37

made RequestDataHolder classes configurable. Deprecated getModuleAccessor, getActionAccessor methods in Request. fixed phpdocs. closes #438

src/view/AgaviView.class.php
@@ -330,7 +330,7 @@ abstract class AgaviView
 			$l = $this->createLayer($layer['class'], $name, $layer['renderer']);
 			$l->setParameters($layer['parameters']);
 			foreach($layer['slots'] as $slotName => $slot) {
-				$l->setSlot($slotName, $this->createSlotContainer($slot['module'], $slot['action'], new AgaviRequestDataHolder(array('parameters' => $slot['parameters'])), $slot['output_type']));
+				$l->setSlot($slotName, $this->createSlotContainer($slot['module'], $slot['action'], array('parameters' => $slot['parameters']), $slot['output_type']));
 			}
 			$this->appendLayer($l);
 		}
@@ -338,20 +338,60 @@ abstract class AgaviView
 		return $layout['parameters'];
 	}
 
+	/**
+	 * Creates a new container with the same output type as this view's container.
+	 *
+	 * This container will have a parameter called 'is_slot' set to true.
+	 *
+	 * @param      string The name of the module.
+	 * @param      string The name of the action.
+	 * @param      mixeed An AgaviRequestDataHolder instance with additional
+	 *                    request arguments or an array of request parameters.
+	 * @param      string Optional name of an initial output type to set.
+	 *
+	 * @return     AgaviExecutionContainer A new execution container instance,
+	 *                                     fully initialized.
+	 *
+	 * @see        AgaviExecutionContainer::createExecutionContainer()
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      0.11.0
+	 */
 	public function createSlotContainer($moduleName, $actionName, $arguments = array(), $outputType = null)
 	{
 		if(!($arguments instanceof AgaviRequestDataHolder)) {
-			$arguments = new AgaviRequestDataHolder(array(AgaviRequestDataHolder::SOURCE_PARAMETERS => $arguments));
+			$rdhc = $this->context->getRequest()->getParameter('request_data_holder_class');
+			$arguments = new $rdhc(array(AgaviRequestDataHolder::SOURCE_PARAMETERS => $arguments));
 		}
 		$container = $this->container->createExecutionContainer($moduleName, $actionName, $arguments, $outputType);
 		$container->setParameter('is_slot', true);
 		return $container;
 	}
 
+	/**
+	 * Creates a new container with the same output type as this view's container.
+	 *
+	 * This container will have a parameter called 'is_forward' set to true.
+	 *
+	 * @param      string The name of the module.
+	 * @param      string The name of the action.
+	 * @param      mixeed An AgaviRequestDataHolder instance with additional
+	 *                    request arguments or an array of request parameters.
+	 * @param      string Optional name of an initial output type to set.
+	 *
+	 * @return     AgaviExecutionContainer A new execution container instance,
+	 *                                     fully initialized.
+	 *
+	 * @see        AgaviExecutionContainer::createExecutionContainer()
+	 *
+	 * @author     David Zülke <dz@bitxtender.com>
+	 * @since      0.11.0
+	 */
 	public function createForwardContainer($moduleName, $actionName, $arguments = array(), $outputType = null)
 	{
 		if(!($arguments instanceof AgaviRequestDataHolder)) {
-			$arguments = new AgaviRequestDataHolder(array(AgaviRequestDataHolder::SOURCE_PARAMETERS => $arguments));
+			$rdhc = $this->context->getRequest()->getParameter('request_data_holder_class');
+			$arguments = new $rdhc(array(AgaviRequestDataHolder::SOURCE_PARAMETERS => $arguments));
 		}
 		$container = $this->container->createExecutionContainer($moduleName, $actionName, $arguments, $outputType);
 		$container->setParameter('is_forward', true);

src/controller/AgaviExecutionContainer.class.php
@@ -284,7 +284,8 @@ class AgaviExecutionContainer extends AgaviAttributeHolder
 				if($this->arguments !== null) {
 					$this->requestData = $this->arguments;
 				} else {
-					$this->requestData = new AgaviRequestDataHolder();
+					$rdhc = $request->getParameter('request_data_holder_class');
+					$this->requestData = new $rdhc();
 				}
 				// run the execution filter, without a proper chain
 				$controller->getFilter('execution')->execute(new AgaviFilterChain(), $this);

src/request/AgaviRequest.class.php
@@ -57,16 +57,6 @@ abstract class AgaviRequest extends AgaviAttributeHolder
 	protected $requestData = null;
 
 	/**
-	 * @var        string The module accessor name.
-	 */
-	protected $moduleAccessor = 'module';
-
-	/**
-	 * @var        string The action accessor name.
-	 */
-	protected $actionAccessor = 'action';
-
-	/**
 	 * @var        bool A boolean value indicating whether or not the request is 
 	 *                  locked.
 	 */
@@ -100,6 +90,21 @@ abstract class AgaviRequest extends AgaviAttributeHolder
 	}
 
 	/**
+	 * Constructor.
+	 *
+	 * @author     David Zuelke <dz@bitxtender.com>
+	 * @since      0.11.0
+	 */
+	public function __construct()
+	{
+		$this->setParameters(array(
+			'module_accessor' => 'module',
+			'action_accessor' => 'action',
+			'request_data_holder_class' => 'AgaviRequestDataHolder',
+		));
+	}
+
+	/**
 	 * Initialize this Request.
 	 *
 	 * @param      AgaviContext An AgaviContext instance.
@@ -115,18 +120,12 @@ abstract class AgaviRequest extends AgaviAttributeHolder
 	{
 		$this->context = $context;
 		
-		$this->parameters = $parameters;
-		
 		if(isset($parameters['default_namespace'])) {
 			$this->defaultNamespace = $parameters['default_namespace'];
+			unset($parameters['default_namespace']);
 		}
 		
-		if(isset($parameters['module_accessor'])) {
-			$this->moduleAccessor = $parameters['module_accessor'];
-		}
-		if(isset($parameters['action_accessor'])) {
-			$this->actionAccessor = $parameters['action_accessor'];
-		}
+		$this->setParameters($parameters);
 	}
 
 	/**
@@ -148,12 +147,14 @@ abstract class AgaviRequest extends AgaviAttributeHolder
 	 *
 	 * @return     string The module accessor name.
 	 *
+	 * @deprecated Use getParameter('module_accessor') instead.
+	 *
 	 * @author     David Zülke <dz@bitxtender.com>
 	 * @since      0.11.0
 	 */
 	public function getModuleAccessor()
 	{
-		return $this->moduleAccessor;
+		return $this->getParameter('module_accessor');
 	}
 
 	/**
@@ -161,12 +162,14 @@ abstract class AgaviRequest extends AgaviAttributeHolder
 	 *
 	 * @return     string The action accessor name.
 	 *
+	 * @deprecated Use getParameter('action_accessor') instead.
+	 *
 	 * @author     David Zülke <dz@bitxtender.com>
 	 * @since      0.11.0
 	 */
 	public function getActionAccessor()
 	{
-		return $this->actionAccessor;
+		return $this->getParameter('action_accessor');
 	}
 
 	/**

src/request/AgaviWebRequest.class.php
@@ -188,6 +188,20 @@ class AgaviWebRequest extends AgaviRequest
 	}
 	
 	/**
+	 * Constructor.
+	 *
+	 * @author     David Zuelke <dz@bitxtender.com>
+	 * @since      0.11.0
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->setParameters(array(
+			'request_data_holder_class' => 'AgaviWebRequestDataHolder',
+		));
+	}
+
+	/**
 	 * Initialize this Request.
 	 *
 	 * @param      AgaviContext An AgaviContext instance.
@@ -294,11 +308,12 @@ class AgaviWebRequest extends AgaviRequest
 			}
 		}
 		
-		$this->requestData = new AgaviWebRequestDataHolder(array(
-			AgaviWebRequestDataHolder::SOURCE_PARAMETERS => array_merge($_GET, $_POST),
-			AgaviWebRequestDataHolder::SOURCE_COOKIES => $_COOKIE,
-			AgaviWebRequestDataHolder::SOURCE_FILES => $_FILES,
-			AgaviWebRequestDataHolder::SOURCE_HEADERS => $headers,
+		$rdhc = $this->getParameter('request_data_holder_class');
+		$this->requestData = new $rdhc(array(
+			constant("$rdhc::SOURCE_PARAMETERS") => array_merge($_GET, $_POST),
+			constant("$rdhc::SOURCE_COOKIES") => $_COOKIE,
+			constant("$rdhc::SOURCE_FILES") => $_FILES,
+			constant("$rdhc::SOURCE_HEADERS") => $headers,
 		));
 	}
 	

src/translation/AgaviTranslationManager.class.php
@@ -584,7 +584,7 @@ class AgaviTranslationManager
 	/**
 	 * @see        AgaviTranslationManager::getLocaleIdentifier
 	 *
-	 * @deprecated
+	 * @deprecated Superseded by AgaviTranslationManager::getLocaleIdentifier()
 	 */
 	public function getClosestMatchingLocale($identifier)
 	{
@@ -653,7 +653,7 @@ class AgaviTranslationManager
 	/**
 	 * @see        AgaviTranslationManager::getLocale
 	 *
-	 * @deprecated
+	 * @deprecated Superseded by AgaviTranslationManager::getLocale()
 	 */
 	public function getLocaleFromIdentifier($identifier)
 	{

commit 6c2b5e1fbf67d83d42613b57c655c79fab31576f

fixed small bug introduced in ec12c880c4ef722d1befc6e88adf3de1d6edac37, refs #438

src/view/AgaviView.class.php
@@ -330,7 +330,7 @@ abstract class AgaviView
 			$l = $this->createLayer($layer['class'], $name, $layer['renderer']);
 			$l->setParameters($layer['parameters']);
 			foreach($layer['slots'] as $slotName => $slot) {
-				$l->setSlot($slotName, $this->createSlotContainer($slot['module'], $slot['action'], array('parameters' => $slot['parameters']), $slot['output_type']));
+				$l->setSlot($slotName, $this->createSlotContainer($slot['module'], $slot['action'], $slot['parameters'], $slot['output_type']));
 			}
 			$this->appendLayer($l);
 		}