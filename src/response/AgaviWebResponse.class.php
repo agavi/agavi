d493e0e0429045492d5d41aafaef4e2860730140
David Zülke <david.zuelke@bitextender.com>
2007-11-02 15:15:37 +0000

    Send HTTP/1.0 responses if request was HTTP/1.0, closes #604, based on Felix' patch.

--- src/response/AgaviWebResponse.class.php
+++ src/response/AgaviWebResponse.class.php
@@ -30,9 +30,49 @@
 class AgaviWebResponse extends AgaviResponse
 {
 	/**
-	 * @var        array An array of all HTTP status codes and their message.
+	 * @var        array An array of all HTTP 1.0 status codes and their message.
 	 */
-	protected $httpStatusCodes = array(
+	protected $http10StatusCodes = array(
+		'200' => "HTTP/1.0 200 OK",
+		'201' => "HTTP/1.0 201 Created",
+		'202' => "HTTP/1.0 202 Accepted",
+		'204' => "HTTP/1.0 204 No Content",
+		'205' => "HTTP/1.0 205 Reset Content",
+		'206' => "HTTP/1.0 206 Partial Content",
+		'300' => "HTTP/1.0 300 Multiple Choices",
+		'301' => "HTTP/1.0 301 Moved Permanently",
+		'302' => "HTTP/1.0 302 Found",
+		'304' => "HTTP/1.0 304 Not Modified",
+		'400' => "HTTP/1.0 400 Bad Request",
+		'401' => "HTTP/1.0 401 Unauthorized",
+		'402' => "HTTP/1.0 402 Payment Required",
+		'403' => "HTTP/1.0 403 Forbidden",
+		'404' => "HTTP/1.0 404 Not Found",
+		'405' => "HTTP/1.0 405 Method Not Allowed",
+		'406' => "HTTP/1.0 406 Not Acceptable",
+		'407' => "HTTP/1.0 407 Proxy Authentication Required",
+		'408' => "HTTP/1.0 408 Request Timeout",
+		'409' => "HTTP/1.0 409 Conflict",
+		'410' => "HTTP/1.0 410 Gone",
+		'411' => "HTTP/1.0 411 Length Required",
+		'412' => "HTTP/1.0 412 Precondition Failed",
+		'413' => "HTTP/1.0 413 Request Entity Too Large",
+		'414' => "HTTP/1.0 414 Request-URI Too Long",
+		'415' => "HTTP/1.0 415 Unsupported Media Type",
+		'416' => "HTTP/1.0 416 Requested Range Not Satisfiable",
+		'417' => "HTTP/1.0 417 Expectation Failed",
+		'500' => "HTTP/1.0 500 Internal Server Error",
+		'501' => "HTTP/1.0 501 Not Implemented",
+		'502' => "HTTP/1.0 502 Bad Gateway",
+		'503' => "HTTP/1.0 503 Service Unavailable",
+		'504' => "HTTP/1.0 504 Gateway Timeout",
+		'505' => "HTTP/1.0 505 HTTP Version Not Supported",
+	);
+	
+	/**
+	 * @var        array An array of all HTTP 1.1 status codes and their message.
+	 */
+	protected $http11StatusCodes = array(
 		'100' => "HTTP/1.1 100 Continue",
 		'101' => "HTTP/1.1 101 Switching Protocols",
 		'200' => "HTTP/1.1 200 OK",
@@ -76,6 +116,11 @@ class AgaviWebResponse extends AgaviResponse
 	);
 	
 	/**
+		* @var        array The array with the HTTP status codes to be used here.
+		*/
+	protected $httpStatusCodes = null;
+	
+	/**
 	 * @var        string The HTTP status code to send for the response.
 	 */
 	protected $httpStatusCode = '200';
@@ -115,6 +160,14 @@ class AgaviWebResponse extends AgaviResponse
 			'cookie_secure'   => isset($parameters['cookie_secure'])   ? $parameters['cookie_secure']   : false,
 			'cookie_httponly' => isset($parameters['cookie_httponly']) ? $parameters['cookie_httponly'] : false,
 		));
+		
+		switch($context->getRequest()->getProtocol()) {
+			case 'HTTP/1.1':
+				$this->httpStatusCodes = $this->http11StatusCodes;
+				break;
+			default:
+				$this->httpStatusCodes = $this->http10StatusCodes;
+		}
 	}
 	
 	/**
@@ -255,7 +308,7 @@ class AgaviWebResponse extends AgaviResponse
 		if(isset($this->httpStatusCodes[$code])) {
 			$this->httpStatusCode = $code;
 		} else {
-			throw new AgaviException('Invalid HTTP Status code: ' . $code);
+			throw new AgaviException(sprintf('Invalid %s Status code: %s', $this->context->getRequest()->getProtocol(), $code));
 		}
 	}
 	
