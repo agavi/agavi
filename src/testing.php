490508152045c086aba8fd096084607f0ca94bf5
David Zülke <david.zuelke@bitextender.com>
2007-12-21 23:12:23 +0000

    require PHP 5.3.0-dev for the testing stuff

--- src/testing.php
+++ src/testing.php
@@ -5,6 +5,12 @@ $here = realpath(dirname(__FILE__));
 // load Agavi basics
 require_once($here . '/agavi.php');
 
+// check minimum PHP version
+AgaviConfig::set('core.minimum_php_version', '5.3.0-dev');
+if(!version_compare(PHP_VERSION, AgaviConfig::get('core.minimum_php_version'), 'ge') ) {
+	die('You must be using PHP version ' . AgaviConfig::get('core.minimum_php_version') . ' or greater.');
+}
+
 // AgaviTesting class
 require_once($here . '/testing/AgaviTesting.class.php');
 
