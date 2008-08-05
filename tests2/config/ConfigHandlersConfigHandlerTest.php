4909a613fa453d2165cda75c384b0c11021570d2
Noah Fontes <nfontes@cynigram.com>
2008-08-05 13:23:49 +0000

    branches/david-xml_only_config_system (refs #519): Update unit tests

--- tests2/config/ConfigHandlersConfigHandlerTest.php
+++ tests2/config/ConfigHandlersConfigHandlerTest.php
@@ -28,14 +28,41 @@ class ConfigHandlersConfigHandlerTest extends ConfigHandlerTestBase
 		$hf = AgaviToolkit::normalizePath(AgaviConfig::get('core.config_dir') . '/routing.xml');
 		$CHCH = new AgaviConfigHandlersConfigHandler();
 
-		$file = $this->getIncludeFile($CHCH->execute(AgaviConfig::get('core.config_dir') . '/tests/config_handlers.xml'));
+		$document = AgaviXmlConfigParser::run(
+			AgaviConfig::get('core.config_dir') . '/tests/config_handlers.xml',
+			'',
+			'',
+			array(
+				AgaviXmlConfigParser::STAGE_SINGLE => array(
+					AgaviConfig::get('core.agavi_dir') . '/config/xsl/config_handlers.xsl',
+				),
+				AgaviXmlConfigParser::STAGE_COMPILATION => array(
+				),
+			),
+			array(
+				AgaviXmlConfigParser::STAGE_SINGLE => array(
+					AgaviXmlConfigParser::STEP_TRANSFORMATIONS_BEFORE => array(
+					),
+					AgaviXmlConfigParser::STEP_TRANSFORMATIONS_AFTER => array(
+						AgaviXmlConfigParser::VALIDATION_TYPE_RELAXNG => array(
+							AgaviConfig::get('core.agavi_dir') . '/config/rng/config_handlers.rng',
+						),
+					),
+				),
+				AgaviXmlConfigParser::STAGE_COMPILATION => array(
+					AgaviXmlConfigParser::STEP_TRANSFORMATIONS_BEFORE => array(),
+					AgaviXmlConfigParser::STEP_TRANSFORMATIONS_AFTER => array()
+				),
+			)
+		);
+		$file = $this->getIncludeFile($CHCH->execute($document));
 		include($file);
 		unlink($file);
 
 		$this->assertSame(1, count(self::$handlers));
 		$this->assertTrue(isset(self::$handlers[$hf]));
 		$this->assertSame('CHCHTestHandler', self::$handlers[$hf]['class']);
-		$this->assertSame(AgaviConfig::get('core.agavi_dir') . '/config/xsd/routing.xsd', self::$handlers[$hf]['validations']['xml_schema'][0]);
+		$this->assertSame(AgaviConfig::get('core.agavi_dir') . '/config/xsd/routing.xsd', self::$handlers[$hf]['validations']['single']['after-transformations']['xml_schema'][0]);
 		$this->assertSame(array('foo' => 'bar', 'dir' => AgaviConfig::get('core.agavi_dir')) , self::$handlers[$hf]['parameters']);
 	}
 
