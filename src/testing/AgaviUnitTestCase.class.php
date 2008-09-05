0b7bf0476b9abec2cebf40a6cb015eb9496705cc
Felix Gilcher <felix.gilcher@asquera.de>
2008-09-05 09:49:51 +0000

    - always run unit tests in separate processes, - remove the call to set the template

--- src/testing/AgaviUnitTestCase.class.php
+++ src/testing/AgaviUnitTestCase.class.php
@@ -2,11 +2,7 @@
 
 abstract class AgaviUnitTestCase extends PHPUnit_Framework_TestCase implements AgaviIUnitTestCase
 {
-	/**
-	 * @var        bool Indicates to PHPUnit whether or not each test case in this suite should run in isolation. In this case, we want all methods of the test case to run in the same process, but the entire test case run in the suite should be isolated from others, so this must be set to true.
-	 */
-	protected $runInSeparateProcess = true;
-	
+
     /**
      * Constructs a test case with the given name.
      *
@@ -17,12 +13,7 @@ abstract class AgaviUnitTestCase extends PHPUnit_Framework_TestCase implements A
     public function __construct($name = NULL, array $data = array(), $dataName = '')
     {
 		parent::__construct($name, $data, $dataName);
-		$this->methodTemplateName = sprintf(
-            '%1$s%2$stemplates%2$sAgaviTestMethod.tpl',
-
-            dirname(__FILE__),
-            DIRECTORY_SEPARATOR
-          );
+		$this->setRunTestInSeparateProcess(true);
 	}
 	
 	public function getContext()
