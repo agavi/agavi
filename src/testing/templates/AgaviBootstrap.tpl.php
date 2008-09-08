06792eb71b37fbc0ad8ca7a2383db047e6e2c669
Felix Gilcher <felix.gilcher@asquera.de>
2008-09-08 10:10:29 +0000

    an agavi bootstrap template for PHPUnit

---
+++ src/testing/templates/AgaviBootstrap.tpl.php
@@ -0,0 +1,9 @@
+<?php
+require_once('testing.php');
+AgaviConfig::fromArray($GLOBALS['AGAVI_CONFIG']);
+unset($GLOBALS['AGAVI_CONFIG']);
+
+
+AgaviTesting::bootstrap();
+Agavi::bootstrap(AgaviConfig::get('testing.environment'));
+?>
