c58dd0df4f4daec7a0b3ee8494df26c04b0e7bb4
Felix Gilcher <felix.gilcher@asquera.de>
2008-09-11 08:36:26 +0000

    added testfile for the searchenginespam action

---
+++ samples/test/tests/fragment/SearchEngineSpamActionTest.php
@@ -0,0 +1,49 @@
+<?php 
+
+class SearchEngineSpamActionTest extends AgaviActionTestCase
+{
+	
+    public function __construct($name = NULL, array $data = array(), $dataName = '')
+    {
+		parent::__construct($name, $data, $dataName);
+		$this->actionName = 'SearchEngineSpam';
+		$this->moduleName = 'Default';
+		
+	}
+	
+	/**
+	 * @dataProvider products
+	 */
+	public function testSuccessViewValidProducts($productName)
+	{
+		$this->setRequestMethod('read');
+		$this->setArguments($this->createRequestDataHolder(array('name' => $productName), 'AgaviWebRequestDataHolder'));
+		$this->runAction();
+		$this->assertViewEquals('Success');
+	}
+	
+	public function testErrorViewInvalidProduct()
+	{
+		$this->setRequestMethod('read');
+		$this->setArguments($this->createRequestDataHolder(array('name' => 'nonexistant product'), 'AgaviWebRequestDataHolder'));
+		$this->runAction();
+		$this->assertViewEquals('Error');
+	}
+	
+	public function testIsNotSimple()
+	{
+		$this->assertIsNotSimple();
+	}
+	public function products()
+	{
+		return array(	'brains' 	=> array('brains'),
+						'chainsaws' => array('chainsaws'),
+						'coding'	=> array('mad coding skills'),
+						'nonsense'	=> array('nonsense'),
+						'viagra'	=> array('viagra'));
+	}
+
+
+}
+
+?>
