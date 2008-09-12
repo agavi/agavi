36d7ffca29254799a1a171a252a2e791cabcf322
Felix Gilcher <felix.gilcher@asquera.de>
2008-09-12 10:26:28 +0000

    codingstandards cleanup

--- src/testing/AgaviFragmentTestCase.class.php
+++ src/testing/AgaviFragmentTestCase.class.php
@@ -2,15 +2,15 @@
 
 abstract class AgaviFragmentTestCase extends PHPUnit_Framework_TestCase implements AgaviIFragmentTestCase
 {
-    /**
-     * Constructs a test case with the given name.
-     *
-     * @param  string $name
-     * @param  array  $data
-     * @param  string $dataName
-     */
-    public function __construct($name = NULL, array $data = array(), $dataName = '')
-    {
+	/**
+	 * Constructs a test case with the given name.
+	 *
+	 * @param  string $name
+	 * @param  array  $data
+	 * @param  string $dataName
+	 */
+	public function __construct($name = NULL, array $data = array(), $dataName = '')
+	{
 		parent::__construct($name, $data, $dataName);
 		$this->setRunTestInSeparateProcess(true);
 	}
