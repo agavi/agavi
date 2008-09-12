37a1756363d9809e710ad46ad3a7b0fbbbd8e7b2
Felix Gilcher <felix.gilcher@asquera.de>
2008-09-12 13:41:45 +0000

    whitespace change

--- samples/test/tests/unit/PriceFinderModelTest.php
+++ samples/test/tests/unit/PriceFinderModelTest.php
@@ -14,18 +14,19 @@ class PriceFinderModelTest extends AgaviUnitTestCase
 	
 	public function productPrices()
 	{
-		return array(	'brains' 	=> array('brains', 0.89),
-						'chainsaws' => array('chainsaws', 129.99),
-						'coding'	=> array('mad coding skills', 14599),
-						'nonsense'	=> array('nonsense', 3.14),
-						'viagra'	=> array('viagra', 14.69));
+		return array(
+			'brains'    => array('brains', 0.89),
+			'chainsaws' => array('chainsaws', 129.99),
+			'coding'    => array('mad coding skills', 14599),
+			'nonsense'  => array('nonsense', 3.14),
+			'viagra'    => array('viagra', 14.69),
+		);
 	}
 	
 	public function testPriceNullForUnknownProduct()
-    {
+	{
 		$this->assertNull($this->getContext()->getModel('PriceFinder', 'Default')->getPriceByProductName('unknown product'));
 	}
-
 }
 
 ?>
