f8268fab1db4bc95de1c998ebed090bbce4f9cd3
Felix Gilcher <felix.gilcher@asquera.de>
2008-09-19 13:05:19 +0000

    now uses the default contextname if the testcase does not specify one

--- src/testing/AgaviUnitTestCase.class.php
+++ src/testing/AgaviUnitTestCase.class.php
@@ -30,27 +30,24 @@
  */
 abstract class AgaviUnitTestCase extends PHPUnit_Framework_TestCase implements AgaviIUnitTestCase
 {
+	protected $contextName = null;
 
-    /**
-     * Constructs a test case with the given name.
-     *
-     * @param  string $name
-     * @param  array  $data
-     * @param  string $dataName
-     */
-    public function __construct($name = NULL, array $data = array(), $dataName = '')
-    {
+	/**
+	 * Constructs a test case with the given name.
+	 *
+	 * @param  string $name
+	 * @param  array  $data
+	 * @param  string $dataName
+	 */
+	public function __construct($name = NULL, array $data = array(), $dataName = '')
+	{
 		parent::__construct($name, $data, $dataName);
 		$this->setRunTestInSeparateProcess(true);
 	}
-	
+
 	public function getContext()
 	{
-		/**
-		 * @TODO change this implementation, it's only here for dev purposes
-		 */
-		return AgaviContext::getInstance('web');
+		return AgaviContext::getInstance($this->contextName);
 	}
-}
 
-?>
+}
