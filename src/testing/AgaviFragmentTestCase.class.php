fb61791dcbb72c6ff1f5d0e680971e9861c06ab7
Felix Gilcher <felix.gilcher@asquera.de>
2008-09-22 15:54:55 +0000

    moved some more methods up the inheritance tree - all container delegates - methods for creating the ExecutionFilter and ExecutionContainer

--- src/testing/AgaviFragmentTestCase.class.php
+++ src/testing/AgaviFragmentTestCase.class.php
@@ -133,6 +133,281 @@ abstract class AgaviFragmentTestCase extends PHPUnit_Framework_TestCase implemen
 		return $shortName;
 	}
 
+	/**
+	 * create an executionfilter for the test
+	 * 
+	 * the configured executionfilter class will be wrapped in a testing
+	 * extension to provide advanced capabilities required for testing 
+	 * only
+	 * 
+	 * @return     AgaviExecutionFilter 
+	 * 
+	 * @author     Felix Gilcher <felix.gilcher@bitextender.com>
+	 * @since      1.0.0
+	 */
+	protected function createExecutionFilter()
+	{
+		$effi = $this->getContext()->getFactoryInfo('execution_filter');
+
+		$wrapper_class = $effi['class'].'UnitTesting';
+
+		//extend the original class to overwrite runAction, so that the containers request data is cloned
+		if(!class_exists($wrapper_class)) {
+			$code = sprintf('
+class %1$s extends %2$s
+{
+	protected $validationResult = null;
+
+	public function performValidation(AgaviExecutionContainer $container)
+	{	
+		return  parent::performValidation($container);
+	}
+
+	public function runAction(AgaviExecutionContainer $container)
+	{
+		$container->cloneArgumentsToRequestData();
+		return parent::runAction($container);
+	}
+}',
+			$wrapper_class,
+			$effi['class']);
+
+			eval($code);
+		}
+
+		// create a new execution container with the wrapped class
+		$filter = new $wrapper_class();
+		$filter->initialize($this->getContext(), $effi['parameters']);
+		return $filter;
+	}
+
+	/**
+	 * create an AgaviExecutionContainer for the test
+	 * 
+	 * the configured AgaviExecutionContainer class will be wrapped in a testing
+	 * extension to provide advanced capabilities required for testing 
+	 * only
+	 * 
+	 * @return     AgaviExecutionContainer 
+	 * 
+	 * @author     Felix Gilcher <felix.gilcher@bitextender.com>
+	 * @since      1.0.0
+	 */
+	protected function createExecutionContainer()
+	{
+		$context = $this->getContext();
+
+		$ecfi = $context->getFactoryInfo('execution_container');
+		$wrapper_class = $ecfi['class'].'UnitTesting';
+
+		//extend the original class to add a setter for the action instance
+		if(!class_exists($wrapper_class)) {
+			$code = sprintf('
+class %1$s extends %2$s
+{
+	protected $validationResult = null;
+
+	public function performValidation()
+	{	
+		if(null === $this->validationResult) {
+			$this->cloneArgumentsToRequestData();
+			$this->validationResult = parent::performValidation();
+		}
+		return $this->validationResult;
+	}
+
+	public function cloneArgumentsToRequestData()
+	{
+		$this->requestData = clone $this->arguments;
+	}
+
+	public function setActionInstance(AgaviAction $action)
+	{
+		$this->actionInstance = $action;
+	}
+}',
+			$wrapper_class,
+			$ecfi['class']);
+
+			eval($code);
+		}
+
+		// create a new execution container with the wrapped class
+		$container = new $wrapper_class();
+		$container->initialize($context, $ecfi['parameters']);
+		$container->setModuleName($this->moduleName);
+		$container->setActionName($this->actionName);
+		$container->setArguments($this->createRequestDataHolder(array()));
+
+		return $container;
+	}
+
+	/* --- container delegates --- */
+
+	/**
+	 * @see        AgaviExcutionContainer::setOutputType()
+	 *
+	 * @author     Felix Gilcher <felix.gilcher@bitextender.com>
+	 * @since      1.0.0
+	 */
+	protected function setOutputType(AgaviOutputType $outputType)
+	{
+		$this->container->setOutputType($outputType);
+	}
+
+	/**
+	 * @see        AgaviExcutionContainer::setArguments()
+	 *
+	 * @author     Felix Gilcher <felix.gilcher@bitextender.com>
+	 * @since      1.0.0
+	 */
+	protected function setArguments(AgaviRequestDataHolder $rd)
+	{
+		$this->container->setArguments($rd);
+	}
+
+	/**
+	 * @see        AgaviExcutionContainer::setRequestMethod()
+	 *
+	 * @author     Felix Gilcher <felix.gilcher@bitextender.com>
+	 * @since      1.0.0
+	 */
+	protected function setRequestMethod($method)
+	{
+		$this->container->setRequestMethod($method);
+	}
+
+	/**
+	 * @see        AgaviAttributeHolder::clearAttributes()
+	 *
+	 * @author     Felix Gilcher <felix.gilcher@bitextender.com>
+	 * @since      1.0.0
+	 */
+	protected function clearAttributes()
+	{
+		$this->container->clearAttributes();
+	}
+
+	/**
+	 * @see        AgaviAttributeHolder::getAttribute()
+	 *
+	 * @author     Felix Gilcher <felix.gilcher@bitextender.com>
+	 * @since      1.0.0
+	 */
+	protected function &getAttribute($name, $default = null)
+	{
+		return $this->container->getAttribute($name, null, $default);
+	}
+
+	/**
+	 * @see        AgaviAttributeHolder::getAttributeNames()
+	 *
+	 * @author     Felix Gilcher <felix.gilcher@bitextender.com>
+	 * @since      1.0.0
+	 */
+	protected function getAttributeNames()
+	{
+		return $this->container->getAttributeNames();
+	}
+
+	/**
+	 * @see        AgaviAttributeHolder::getAttributes()
+	 *
+	 * @author     Felix Gilcher <felix.gilcher@bitextender.com>
+	 * @since      1.0.0
+	 */
+	protected function &getAttributes()
+	{
+		return $this->container->getAttributes();
+	}
+
+	/**
+	 * @see        AgaviAttributeHolder::hasAttribute()
+	 *
+	 * @author     Felix Gilcher <felix.gilcher@bitextender.com>
+	 * @since      1.0.0
+	 */
+	protected function hasAttribute($name)
+	{
+		return $this->container->hasAttribute($name);
+	}
+
+	/**
+	 * @see        AgaviAttributeHolder::removeAttribute()
+	 *
+	 * @author     Felix Gilcher <felix.gilcher@bitextender.com>
+	 * @since      1.0.0
+	 */
+	protected function &removeAttribute($name)
+	{
+		return $this->container->removeAttribute($name);
+	}
+
+	/**
+	 * @see        AgaviAttributeHolder::setAttribute()
+	 *
+	 * @author     Felix Gilcher <felix.gilcher@bitextender.com>
+	 * @since      1.0.0
+	 */
+	protected function setAttribute($name, $value)
+	{
+		$this->container->setAttribute($name, $value);
+	}
+
+	/**
+	 * @see        AgaviAttributeHolder::appendAttribute()
+	 *
+	 * @author     Felix Gilcher <felix.gilcher@bitextender.com>
+	 * @since      1.0.0
+	 */
+	protected function appendAttribute($name, $value)
+	{
+		$this->container->appendAttribute($name, $value);
+	}
+
+	/**
+	 * @see        AgaviAttributeHolder::setAttributesByRef()
+	 *
+	 * @author     Felix Gilcher <felix.gilcher@bitextender.com>
+	 * @since      1.0.0
+	 */
+	protected function setAttributeByRef($name, &$value)
+	{
+		$this->container->setAttributeByRef($name, $value);
+	}
+
+	/**
+	 * @see        AgaviAttributeHolder::appendAttributeByRef()
+	 *
+	 * @author     Felix Gilcher <felix.gilcher@bitextender.com>
+	 * @since      1.0.0
+	 */
+	protected function appendAttributeByRef($name, &$value)
+	{
+		$this->container->appendAttributeByRef($name, $value);
+	}
+
+	/**
+	 * @see        AgaviAttributeHolder::setAttributes()
+	 *
+	 * @author     Felix Gilcher <felix.gilcher@bitextender.com>
+	 * @since      1.0.0
+	 */
+	protected function setAttributes(array $attributes)
+	{
+		$this->container->setAttributes($attributes);
+	}
+
+	/**
+	 * @see        AgaviAttributeHolder::setAttributesByRef()
+	 *
+	 * @author     Felix Gilcher <felix.gilcher@bitextender.com>
+	 * @since      1.0.0
+	 */
+	protected function setAttributesByRef(array &$attributes)
+	{
+		$this->container->setAttributesByRef($attributes);
+	}
 }
 
 ?>
