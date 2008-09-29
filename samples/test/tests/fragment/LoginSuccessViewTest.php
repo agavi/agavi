e142b823b64644241079b45f2bfe5da2cdacabcc
Felix Gilcher <felix.gilcher@asquera.de>
2008-09-29 17:23:03 +0000

    adds a test for the html response without redirect

--- samples/test/tests/fragment/LoginSuccessViewTest.php
+++ samples/test/tests/fragment/LoginSuccessViewTest.php
@@ -16,13 +16,25 @@ class LoginSuccessViewTest extends AgaviViewTestCase
 		$this->assertHandlesOutputType('html');
 	}
 	
-	public function testResponseHtml()
-	{		
+	public function testResponseRedirect()
+	{
 		$this->setArguments($this->createRequestDataHolder(array(AgaviWebRequestDataHolder::SOURCE_PARAMETERS => array('username' => 'foo', 'password' => 'bar'))));
 		$this->getContext()->getUser()->setAttribute('redirect', 'http://www.example.com/', 'org.agavi.SampleApp.login');
 		$this->runView();
 		$this->assertViewResultEquals('');
 		$this->assertResponseRedirectsTo(array('code' => '302', 'location' => 'http://www.example.com/'));
+	}
+	
+	public function testResponseHtml()
+	{
+		$this->setArguments($this->createRequestDataHolder(array(AgaviWebRequestDataHolder::SOURCE_PARAMETERS => array('username' => 'foo', 'password' => 'bar'))));
+		$this->runView();
+		$this->assertResponseHasHTTPStatus(200);
+		$this->assertViewResultEquals('');
+		$this->assertHasLayer('content');
+		$this->assertHasLayer('decorator');
+		$this->assertResponseHasNoRedirect();
+		$this->assertContainerAttributeExists('title');
 	}
 }
 
