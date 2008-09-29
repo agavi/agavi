438e209ec70814d25fbeaaacd94523b3af46856a
Felix Gilcher <felix.gilcher@asquera.de>
2008-09-29 16:43:37 +0000

    added a sample testcase for the LoginSuccessView

---
+++ samples/test/tests/fragment/LoginSuccessViewTest.php
@@ -0,0 +1,29 @@
+<?php 
+
+class LoginSuccessViewTest extends AgaviViewTestCase
+{
+
+	public function __construct($name = NULL, array $data = array(), $dataName = '')
+	{
+		parent::__construct($name, $data, $dataName);
+		$this->actionName = 'Login';
+		$this->moduleName = 'Default';
+		$this->viewName   = 'Success';
+	}
+	
+	public function testHandlesOutputType()
+	{
+		$this->assertHandlesOutputType('html');
+	}
+	
+	public function testResponseHtml()
+	{		
+		$this->setArguments($this->createRequestDataHolder(array(AgaviWebRequestDataHolder::SOURCE_PARAMETERS => array('username' => 'foo', 'password' => 'bar'))));
+		$this->getContext()->getUser()->setAttribute('redirect', 'http://www.example.com/', 'org.agavi.SampleApp.login');
+		$this->runView();
+		$this->assertViewResultEquals('');
+		$this->assertResponseRedirectsTo(array('code' => '302', 'location' => 'http://www.example.com/'));
+	}
+}
+
+?>
