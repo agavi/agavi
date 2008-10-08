17c3f2688d268cedf3c272aff1a4ec6bfc073f4c
Felix Gilcher <felix.gilcher@asquera.de>
2008-10-08 15:26:14 +0000

    now asserts that there is a proper headline tag in the response

--- samples/test/tests/flow/LoginFlowTest.php
+++ samples/test/tests/flow/LoginFlowTest.php
@@ -15,6 +15,7 @@ class LoginFlowTest extends AgaviFlowTestCase
 		$this->setArguments($this->createRequestDataHolder(array(AgaviWebRequestDataHolder::SOURCE_PARAMETERS => array('username' => 'Chuck Norris', 'password' => 'kick'))));
 		$this->dispatch();
 		$this->assertResponseHasTag(array('tag' => 'body'));
+		$this->assertResponseHasTag(array('tag' => 'h2', 'content' => 'Login Successful'));
 	}
 	
 }
