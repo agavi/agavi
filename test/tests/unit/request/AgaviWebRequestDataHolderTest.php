52c4c2a325df971988ef042c4c28f94ef1ca7f4b
Felix Gilcher <felix.gilcher@asquera.de>
2008-12-10 22:34:19 +0000

    adds some tests for the webrequestdataholder
    - sort of refs #955 #956

--- test/tests/unit/request/AgaviWebRequestDataHolderTest.php
+++ test/tests/unit/request/AgaviWebRequestDataHolderTest.php
@@ -10,6 +10,63 @@ class AgaviWebRequestDataHolderTest extends AgaviPhpUnitTestCase
 		$this->assertEquals('bar', $dh->removeCookie('nested[foo]'), 'Failed asserting that the return value is "bar" when removing nested[foo].');
 		$this->$this->assertFalse($dh->hasCookie('nested[foo]'), 'Failed asserting that the nested cookie part "foo" was removed.');
 	}
+	
+	public function testNullParameterValue()
+	{
+		$dh = new AgaviWebRequestDataHolder(array(AgaviWebRequestDataHolder::SOURCE_PARAMETERS => array('nullvalue' => null)));
+		$this->assertTrue($dh->hasParameter('nullvalue'), 'Failed asserting that the dataholder has a parameter named "nullvalue".');
+		$this->assertTrue(
+			$dh->isParameterValueEmpty('nullvalue'), 
+			'Failed asserting that the dh->isParameterValueEmpty() returns true on the parameter named "nullvalue".'
+		);
+		$this->assertEquals(null, $dh->getParameter('nullvalue'), 'Failed asserting that the parameter named "nullvalue" has the value "null".');
+	}
+	
+	/**
+	 * @dataProvider dataTestParameterSet
+	 */ 
+	public function testSetParameter($setKey, $setValue, $readKey, $readValue)
+	{
+		$dh = new AgaviWebRequestDataHolder(array());
+		$dh->setParameter($setKey, $setValue);
+		$this->assertEquals($readValue, $dh->getParameter($readKey));
+	}
+	
+	public function dataTestParameterSet()
+	{
+		return array(
+			array(
+				'flat',
+				'flatvalue',
+				'flat',
+				'flatvalue',
+			),
+			array(
+				'nested',
+				array(0 => 1),
+				'nested[0]',
+				1,
+			),
+			array(
+				'nested',
+				array('foo' => 'bar'),
+				'nested[foo]',
+				'bar',
+			),
+			array(
+				'nested',
+				array(0 => 1),
+				'nested',
+				array(0 => 1),
+			),
+			array(
+				'nested',
+				array('foo' => 'bar'),
+				'nested',
+				array('foo' => 'bar'),
+			),
+		);
+	}
 }
 
 ?>
