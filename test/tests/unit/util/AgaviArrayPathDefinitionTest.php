cc7deaf6f2caae2d4fea964ea32a0dfa23eb4045
Felix Gilcher <felix.gilcher@asquera.de>
2008-12-14 21:47:53 +0000

    - adds a testcase for the AgaviArrayPathDefinition - adds a set of tests for AgaviArrayPathDefinition::getPartsFromName - refs #308

---
+++ test/tests/unit/util/AgaviArrayPathDefinitionTest.php
@@ -0,0 +1,114 @@
+<?php
+
+class AgaviArrayPathDefintionTest extends AgaviPhpUnitTestCase
+{
+	
+	/**
+	 * @dataProvider getPathPartData
+	 */
+	public function testGetPartsFromPath($path, $expected, $expectedException)
+	{
+		if (!empty($expectedException))
+		{
+			$this->setExpectedException($expectedException);
+		}
+		$this->assertEquals($expected, AgaviArrayPathDefinition::getPartsFromPath($path));
+	}
+	
+	public function getPathPartData()
+	{
+		return array(
+			'absolute,nopath' => array(
+				'level1',
+				array(
+					'parts' => array(
+						'level1',
+					),
+					'absolute' => true,
+				),
+				false,
+			),
+			'absolute,1 level' => array(
+				'absolute[level1]',
+				array(
+					'parts' => array(
+						'absolute',
+						'level1',
+					),
+					'absolute' => true,
+				),
+				false,
+			),
+			'absolute,2 levels' => array(
+				'absolute[level1][level2]',
+				array(
+					'parts' => array(
+						'absolute',
+						'level1',
+						'level2',
+					),
+					'absolute' => true,
+				),
+				false,
+			),
+			'relative, 1 level' => array(
+				'[level1]',
+				array(
+					'parts' => array(
+						'level1'
+					),
+					'absolute' => false,
+				),
+				false,
+			),
+			'relative, 2 levels' => array(
+				'[level1][level2]',
+				array(
+					'parts' => array(
+						'level1',
+						'level2',
+					),
+					'absolute' => false,
+				),
+				false,
+			),
+			'brokenpath-1' => array(
+				'absolute[broken',
+				array(
+					'parts' => array(
+						'absolute',
+						'broken'
+					),
+					'absolute' => true,
+				),
+				'AgaviException',
+			),
+			'brokenpath-2' => array(
+				'absolute[broken]]',
+				array(
+					'parts' => array(
+						'absolute',
+						'broken]'
+					),
+					'absolute' => true,
+				),
+				false,
+			),
+			'brokenpath-3' => array(
+				'absolute[[broken]',
+				array(
+					'parts' => array(
+						'absolute[',
+						'broken'
+					),
+					'absolute' => true,
+				),
+				false,
+			),
+			
+		);
+	}
+}
+
+
+?>
