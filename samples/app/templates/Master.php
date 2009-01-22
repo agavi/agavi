88d1981efade8ce5de16b5e20b135bd541be7c2e
David Zülke <david.zuelke@bitextender.com>
2009-01-22 11:13:59 +0000

    Fixed locale identifier comparison

--- samples/app/templates/Master.php
+++ samples/app/templates/Master.php
@@ -159,7 +159,7 @@ $rtl = ($locale->getCharacterOrientation() == 'right-to-left');
 		<div id="footer">
 			<p><?php echo $tm->__('Alternative language:', 'Alternative languages:', count($availableLocales = $tm->getAvailableLocales()), 'default.layout'); ?>
 <?php foreach($availableLocales as $availableLocale): ?>
-				<a href="<?php echo $ro->gen(null, array('locale' => $availableLocale['identifier'])); ?>" hreflang="<?php echo $availableLocale['identifier']; ?>"<?php if($availableLocale['identifier'] == $locale->getIdentifier()): ?> style="font-weight:bold"<?php endif;?>><?php echo htmlspecialchars($availableLocale['parameters']['description']); ?></a>
+				<a href="<?php echo $ro->gen(null, array('locale' => $availableLocale['identifier'])); ?>" hreflang="<?php echo $availableLocale['identifier']; ?>"<?php if($availableLocale['identifier'] == $tm->getCurrentLocaleIdentifier()): ?> style="font-weight:bold"<?php endif;?>><?php echo htmlspecialchars($availableLocale['parameters']['description']); ?></a>
 <?php endforeach; ?>
 			</p>
 			<p>Copyright © 2005-2009 The Agavi Project</p>
