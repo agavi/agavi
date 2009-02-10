f666f58e5544532660d95aab84f75e8cbc8d38ea
David Zülke <david.zuelke@bitextender.com>
2009-02-10 19:01:08 +0000

    right-align prices

--- samples/app/modules/Products/templates/IndexSuccess.php
+++ samples/app/modules/Products/templates/IndexSuccess.php
@@ -6,7 +6,7 @@
 <?php foreach($t['products'] as $product): ?>
 	<tr>
 		<td><a href="<?php echo $ro->gen('products.product.view', array('id' => $product->getId(), 'name' => $product->getName())); ?>"><?php echo htmlspecialchars($product->getName()); ?></a></td>
-		<td><?php echo $tm->_c($product->getPrice()); ?></td>
+		<td align="right"><?php echo $tm->_c($product->getPrice()); ?></td>
 	</tr>
 <?php endforeach; ?>
 </table>
