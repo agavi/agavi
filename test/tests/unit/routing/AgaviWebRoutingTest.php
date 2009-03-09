893c26b47e8891716fb2545a7311690523fe4078
Dominik del Bondio <dominik.del.bondio@bitextender.com>
2009-03-09 15:49:16 +0000

    add routing regression test refs #1057

--- test/tests/unit/routing/AgaviWebRoutingTest.php
+++ test/tests/unit/routing/AgaviWebRoutingTest.php
@@ -205,6 +205,15 @@ class AgaviWebRoutingTest extends AgaviPhpUnitTestCase
 		$this->assertEquals('/callbacks/foo?id=12345', $url);
 	}
 	
+	public function testGenWithCallbackChangeExtraParam()
+	{
+		$url = $this->routing->gen('callbacks.gen_change_extra_param', array('extra' => 'extra data'));
+		$this->assertEquals('/callbacks/foo?extra=callback+data', $url);
+		$url = $this->routing->gen('callbacks.gen_change_extra_param_routing_value', array('extra' => 'extra data'));
+		$this->assertEquals('/callbacks/foo?extra=callback+data', $url);
+	}
+	
+	
 	public function testGenShortestPossibleUrl()
 	{
 		$url = $this->routing->gen('gen_shortest_possible_url', array(), array('omit_defaults' => true));
