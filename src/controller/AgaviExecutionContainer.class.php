agavi/agavi

commit 0b904d6250b65416120e4b3e40e3b7048d977946

refactors code for cloning the global request data and/or merging the passed arguments in an own method. simplifies testing refs #380

src/controller/AgaviExecutionContainer.class.php
@@ -271,23 +271,13 @@ class AgaviExecutionContainer extends AgaviAttributeHolder
 		// initialize the action
 		$this->actionInstance->initialize($this);
 
+		// copy and merge request data as required
+		$this->initRequestData();
+		
 		if($this->actionInstance->isSimple()) {
-			if($this->arguments !== null) {
-				// clone it so mutating it has no effect on the "outside world"
-				$this->requestData = clone $this->arguments;
-			} else {
-				$rdhc = $request->getParameter('request_data_holder_class');
-				$this->requestData = new $rdhc();
-			}
 			// run the execution filter, without a proper chain
 			$controller->getFilter('execution')->execute(new AgaviFilterChain(), $this);
 		} else {
-			// mmmh I smell awesomeness... clone the RD JIT, yay, that's the spirit
-			$this->requestData = clone $this->globalRequestData;
-
-			if($this->arguments !== null) {
-				$this->requestData->merge($this->arguments);
-			}
 
 			// create a new filter chain
 			$filterChain = $this->context->createInstanceFor('filter_chain');
@@ -317,6 +307,31 @@ class AgaviExecutionContainer extends AgaviAttributeHolder
 		return $this->proceed();
 	}
 	
+	/**
+	 * copies and merges the global request data
+	 * 
+	 * @author       Felix Gilcher <felix.gilcher@bitextender.com>
+	 * @since        1.1.0
+	 */
+	protected function initRequestData() 
+	{
+		if($this->actionInstance->isSimple()) {
+			if($this->arguments !== null) {
+				// clone it so mutating it has no effect on the "outside world"
+				$this->requestData = clone $this->arguments;
+			} else {
+				$rdhc = $request->getParameter('request_data_holder_class');
+				$this->requestData = new $rdhc();
+			}
+		} else {
+			// mmmh I smell awesomeness... clone the RD JIT, yay, that's the spirit
+			$this->requestData = clone $this->globalRequestData;
+
+			if($this->arguments !== null) {
+				$this->requestData->merge($this->arguments);
+			}
+		}
+	}
 	
 	/**
 	 * create a system forward container

commit ce5dc507e0ab363bdaad70544aea566889bc1e01

Fixed bug introduced in 0b904d6250b65416120e4b3e40e3b7048d977946 and merged in c63a5fdf6225177ae9c7b1e397c8fa162e58e414, refs #380

src/controller/AgaviExecutionContainer.class.php
@@ -246,13 +246,10 @@ class AgaviExecutionContainer extends AgaviAttributeHolder
 	{
 		$controller = $this->context->getController();
 
-		$request = $this->context->getRequest();
-
 		$controller->countExecution();
 
 		$moduleName = $this->getModuleName();
 		$actionName = $this->getActionName();
-		
 		
 		try {
 			// TODO: cleanup and merge with createActionInstance once Exceptions have been cleaned up and specced properly so that the two error conditions can be told apart
@@ -313,14 +310,14 @@ class AgaviExecutionContainer extends AgaviAttributeHolder
 	 * @author       Felix Gilcher <felix.gilcher@bitextender.com>
 	 * @since        1.1.0
 	 */
-	protected function initRequestData() 
+	protected function initRequestData()
 	{
 		if($this->actionInstance->isSimple()) {
 			if($this->arguments !== null) {
 				// clone it so mutating it has no effect on the "outside world"
 				$this->requestData = clone $this->arguments;
 			} else {
-				$rdhc = $request->getParameter('request_data_holder_class');
+				$rdhc = $this->getContext()->getRequest()->getParameter('request_data_holder_class');
 				$this->requestData = new $rdhc();
 			}
 		} else {