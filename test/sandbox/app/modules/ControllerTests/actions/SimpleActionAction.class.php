agavi/agavi

commit 0b904d6250b65416120e4b3e40e3b7048d977946

refactors code for cloning the global request data and/or merging the passed arguments in an own method. simplifies testing refs #380

src/controller/AgaviExecutionContainer.class.php
@@ -271,23 +271,13 @@ class AgaviExecutionContainer extends AgaviAttributeHolder
 		// initialize the action
 		$this->actionInstance->initialize($this);
 
+		// copy and merge request data as required
+		$this->initRequestData();
+		
 		if($this->actionInstance->isSimple()) {
-			if($this->arguments !== null) {
-				// clone it so mutating it has no effect on the "outside world"
-				$this->requestData = clone $this->arguments;
-			} else {
-				$rdhc = $request->getParameter('request_data_holder_class');
-				$this->requestData = new $rdhc();
-			}
 			// run the execution filter, without a proper chain
 			$controller->getFilter('execution')->execute(new AgaviFilterChain(), $this);
 		} else {
-			// mmmh I smell awesomeness... clone the RD JIT, yay, that's the spirit
-			$this->requestData = clone $this->globalRequestData;
-
-			if($this->arguments !== null) {
-				$this->requestData->merge($this->arguments);
-			}
 
 			// create a new filter chain
 			$filterChain = $this->context->createInstanceFor('filter_chain');
@@ -317,6 +307,31 @@ class AgaviExecutionContainer extends AgaviAttributeHolder
 		return $this->proceed();
 	}
 	
+	/**
+	 * copies and merges the global request data
+	 * 
+	 * @author       Felix Gilcher <felix.gilcher@bitextender.com>
+	 * @since        1.1.0
+	 */
+	protected function initRequestData() 
+	{
+		if($this->actionInstance->isSimple()) {
+			if($this->arguments !== null) {
+				// clone it so mutating it has no effect on the "outside world"
+				$this->requestData = clone $this->arguments;
+			} else {
+				$rdhc = $request->getParameter('request_data_holder_class');
+				$this->requestData = new $rdhc();
+			}
+		} else {
+			// mmmh I smell awesomeness... clone the RD JIT, yay, that's the spirit
+			$this->requestData = clone $this->globalRequestData;
+
+			if($this->arguments !== null) {
+				$this->requestData->merge($this->arguments);
+			}
+		}
+	}
 	
 	/**
 	 * create a system forward container

commit a70843d4ab2d27370c29ac39ebc87bfcda156fba

adds a testcase for the bug introduced in 0b904d6250b65416120e4b3e40e3b7048d977946, merged in c63a5fdf6225177ae9c7b1e397c8fa162e58e414 and fixed in ce5dc507e0ab363bdaad70544aea566889bc1e01
refs #380

test/config/suites.xml
@@ -12,6 +12,11 @@
 					<testfile>unit/config/AgaviConfigCacheTest.php</testfile>
 				</testfiles>
 			</suite>
+			<suite name="Controller">
+				<testfiles>
+					<testfile>unit/controller/AgaviExecutionContainerTest.php</testfile>
+				</testfiles>
+			</suite>
 			<suite name="Date">
 				<testfiles>
 					<testfile>unit/date/AgaviTimezoneTest.php</testfile>

test/sandbox/app/modules/ControllerTests/actions/SimpleActionAction.class.php
@@ -1,0 +1,29 @@
+<?php
+
+class ControllerTests_SimpleActionAction extends SandboxControllerTestsBaseAction
+{
+	
+	public function isSimple()
+	{
+		return true;
+	}
+	
+	/**
+	 * Returns the default view if the action does not serve the request
+	 * method used.
+	 *
+	 * @return     mixed <ul>
+	 *                     <li>A string containing the view name associated
+	 *                     with this action; or</li>
+	 *                     <li>An array with two indices: the parent module
+	 *                     of the view to be executed and the view to be
+	 *                     executed.</li>
+	 *                   </ul>
+	 */
+	public function getDefaultViewName()
+	{
+		return 'Success';
+	}
+}
+
+?>

test/sandbox/app/modules/ControllerTests/cache/SimpleAction.xml
@@ -1,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ae:configurations xmlns:ae="http://agavi.org/agavi/config/global/envelope/1.0" xmlns="http://agavi.org/agavi/config/parts/caching/1.0">
+	<ae:configuration>
+		<caching>
+		</caching>
+	</ae:configuration>
+</ae:configurations>

test/sandbox/app/modules/ControllerTests/config/autoload.xml
@@ -1,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ae:configurations xmlns="http://agavi.org/agavi/config/parts/autoload/1.0" xmlns:ae="http://agavi.org/agavi/config/global/envelope/1.0">
+	<ae:configuration>
+		
+		<autoload name="SandboxControllerTestsBaseAction">%core.module_dir%/ControllerTests/lib/action/SandboxControllerTestsBaseAction.class.php</autoload>
+		<autoload name="SandboxControllerTestsBaseModel">%core.module_dir%/ControllerTests/lib/model/SandboxControllerTestsBaseModel.class.php</autoload>
+		<autoload name="SandboxControllerTestsBaseView">%core.module_dir%/ControllerTests/lib/view/SandboxControllerTestsBaseView.class.php</autoload>
+		
+	</ae:configuration>
+</ae:configurations>

test/sandbox/app/modules/ControllerTests/config/config_handlers.xml
@@ -1,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ae:configurations xmlns:ae="http://agavi.org/agavi/config/global/envelope/1.0" xmlns="http://agavi.org/agavi/config/parts/config_handlers/1.0">
+	<ae:configuration>
+		<handlers>
+			<handler pattern="%core.module_dir%/*/validate/*.xml" class="AgaviValidatorConfigHandler">
+				<validation>%core.agavi_dir%/config/xsd/validators.xsd</validation>
+				<transformation>%core.agavi_dir%/config/xsl/validators.xsl</transformation>
+			</handler>
+			<handler pattern="%core.module_dir%/*/cache/*.xml" class="AgaviCachingConfigHandler">
+				<validation>%core.agavi_dir%/config/xsd/caching.xsd</validation>
+				<transformation>%core.agavi_dir%/config/xsl/caching.xsl</transformation>
+			</handler>
+		</handlers>
+	</ae:configuration>
+</ae:configurations>

test/sandbox/app/modules/ControllerTests/config/module.xml
@@ -1,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ae:configurations xmlns="http://agavi.org/agavi/config/parts/module/1.0" xmlns:ae="http://agavi.org/agavi/config/global/envelope/1.0">
+	<ae:configuration>
+		
+		<module enabled="true">
+			<settings>
+				<setting name="title">ControllerTests Module</setting>
+				<setting name="version">1.0</setting>
+<!--
+				<setting name="authors">
+					<ae:parameter name="joe.cool@peanuts.com">Snoopy</ae:parameter>
+				</setting>
+				<setting name="homepage">http://www.agavi.org</setting>
+				<setting name="update_url" />
+				<setting name="description">An example module</setting>
+-->
+				<setting name="agavi.action.path">%core.module_dir%/${moduleName}/actions/${actionName}Action.class.php</setting>
+				<setting name="agavi.view.path">%core.module_dir%/${moduleName}/views/${viewName}View.class.php</setting>
+				<setting name="agavi.view.name">${actionName}${viewName}</setting>
+				<setting name="agavi.cache.path">%core.module_dir%/${moduleName}/cache/${actionName}.xml</setting>
+				<setting name="agavi.validate.path">%core.module_dir%/${moduleName}/validate/${actionName}.xml</setting>
+				<setting name="agavi.template.directory">%core.module_dir%/${module}/templates</setting>
+			</settings>
+		</module>
+		
+	</ae:configuration>
+</ae:configurations>

test/sandbox/app/modules/ControllerTests/config/validators.xml
@@ -1,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ae:configurations
+	xmlns="http://agavi.org/agavi/config/parts/validators/1.0"
+	xmlns:ae="http://agavi.org/agavi/config/global/envelope/1.0"
+	parent="%core.config_dir%/validators.xml"
+>
+	<ae:configuration>
+		
+		<validator_definitions>
+		</validator_definitions>
+		
+	</ae:configuration>
+</ae:configurations>

test/sandbox/app/modules/ControllerTests/lib/action/SandboxControllerTestsBaseAction.class.php
@@ -1,0 +1,11 @@
+<?php
+
+/**
+ * The base action from which all ControllerTests module actions inherit.
+ */
+class SandboxControllerTestsBaseAction extends SandboxBaseAction
+{
+
+}
+
+?>

test/sandbox/app/modules/ControllerTests/lib/model/SandboxControllerTestsBaseModel.class.php
@@ -1,0 +1,11 @@
+<?php
+
+/**
+ * The base model from which all ControllerTests module models inherit.
+ */
+class SandboxControllerTestsBaseModel extends SandboxBaseModel
+{
+
+}
+
+?>

test/sandbox/app/modules/ControllerTests/lib/view/SandboxControllerTestsBaseView.class.php
@@ -1,0 +1,11 @@
+<?php
+
+/**
+ * The base view from which all ControllerTests module views inherit.
+ */
+class SandboxControllerTestsBaseView extends SandboxBaseView
+{
+
+}
+
+?>

test/sandbox/app/modules/ControllerTests/validate/SimpleAction.xml
@@ -1,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ae:configurations
+	xmlns="http://agavi.org/agavi/config/parts/validators/1.0"
+	xmlns:ae="http://agavi.org/agavi/config/global/envelope/1.0"
+	parent="%core.module_dir%/ControllerTests/config/validators.xml"
+>
+	<ae:configuration>
+		
+		<validators>
+		</validators>
+		
+	</ae:configuration>
+</ae:configurations>

test/sandbox/app/modules/ControllerTests/views/SimpleActionSuccessView.class.php
@@ -1,0 +1,13 @@
+<?php
+
+class ControllerTests_SimpleActionSuccessView extends SandboxControllerTestsBaseView
+{
+	public function executeHtml(AgaviRequestDataHolder $rd)
+	{
+		$this->setupHtml($rd);
+
+		$this->setAttribute('_title', 'SimpleAction');
+	}
+}
+
+?>

test/tests/unit/controller/AgaviExecutionContainerTest.php
@@ -1,0 +1,12 @@
+<?php
+
+class AgaviExecutionContainerTest extends AgaviUnitTestCase
+{
+	
+	public function testSimpleActionWithoutArguments()
+	{
+		$container = $this->getContext()->getController()->createExecutionContainer('ControllerTests', 'SimpleAction');
+		$response = $container->execute();
+		
+	}
+}