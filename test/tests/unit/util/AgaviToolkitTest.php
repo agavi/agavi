6b59093d922ec67a8065979aa3bd8f5490cba3c0
Felix Gilcher <felix.gilcher@asquera.de>
2010-01-10 10:58:06 +0000

    - added testcases for AgaviToolkit::literalize - copied testcases for AgaviToolkit::isPathAbsolute from the old test - fixed testcases for AgaviToolkit::literalize (backslash is not supported on unix)

--- test/tests/unit/util/AgaviToolkitTest.php
+++ test/tests/unit/util/AgaviToolkitTest.php
@@ -22,4 +22,78 @@ class AgaviToolkitTest extends AgaviPhpUnitTestCase
 					 array('foo', '', '', 0),
 					 array('', 'bar', '', 0));
 	}
+	
+	/**
+	 * @dataProvider literalizeData
+	 */
+	public function testLiteralize($rawValue, $expectedResult, $settings)
+	{
+		foreach($settings as $key => $value) {
+			AgaviConfig::set($key, $value);
+		}
+		
+		$literalized = AgaviToolkit::literalize($rawValue);
+		
+		$this->assertEquals($expectedResult, $literalized);
+	}
+	
+	public function literalizeData()
+	{
+		return array(
+			'(string)true' => array('true', true, array()),
+			'(string)false' => array('false', false, array()),
+			'(string)yes' => array('yes', true, array()),
+			'(string)no' => array('no', false, array()),
+			'(string)on' => array('on', true, array()),
+			'(string)off' => array('off', false, array()),
+			'(string)single space' => array(' ', null, array()),
+			'(string)multiple spaces' => array('    ', null, array()),
+			'(string)newline' => array("\n", null, array()),
+			'(string)newline and space' => array(" \n ", null, array()),
+			'(string)space true space' => array(' true ', true, array()),
+			'(string)%test.replace%' => array('%test.replace%', 'fooo', array('test.replace' => 'fooo')),
+			'(int)5' => array(5, 5, array())
+		);
+	}
+	
+	/**
+	 * @dataProvider pathData
+	 */
+	public function testIsPathAbsolute($path, $expected)
+	{
+		if($expected) {
+			$this->assertTrue(AgaviToolkit::isPathAbsolute($path));
+		} else {
+			$this->assertFalse(AgaviToolkit::isPathAbsolute($path));
+		}
+	}
+	
+	public function pathData()
+	{
+		return array(
+			'c:/' => array('c:/', true),
+			'c:\\' => array('c:\\', true),
+			'c:/Windows' => array('c:/Windows', true),
+			'g:/Windows/bar' => array('g:/Windows/bar', true),
+			'c:\\windows\\foo' => array('c:\\windows\\foo', true),
+			// UNC paths are absolute too
+			'(unc)\\\\some.host' => array('\\\\some.host', true),
+			'(unc)\\\\some.host\\foo' => array('\\\\some.host\\foo', true),
+			'(unc)\\some.host\\foo' => array('\\some.host\\foo', false),
+			
+			'/' => array('/', true),
+			'/root' => array('/root', true),
+			'/FoO/bAR' => array('/FoO/bAR', true),
+			'./FoO/bAR' => array('./FoO/bAR', false),
+			'../FoO/bAR' => array('../FoO/bAR', false),
+			
+			// (php does not support backslashes on *nix)
+			'\\foo' => array('\\foo', false),
+			'\\foo\\bar' => array('\\foo\\bar', false),
+			
+			'c:' => array('c:', false),
+			's/foo/bar' => array('s/foo/bar', false),
+			'c:foo' => array('c:foo', false)
+		);
+	}
 }
