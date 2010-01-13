157c68a8061719411eb2e8e99592f8af508ee843
David Zülke <david.zuelke@bitextender.com>
2010-01-13 14:03:13 +0000

    Fix #1211: ICU Bug 6814

--- src/date/AgaviOlsonTimeZone.class.php
+++ src/date/AgaviOlsonTimeZone.class.php
@@ -442,11 +442,11 @@ class AgaviOlsonTimeZone extends AgaviTimeZone
 		$limit = (int) AgaviCalendarGrego::fieldsToDay($year + 1, 0, 1) * AgaviDateDefinitions::SECONDS_PER_DAY;
 
 		// Return TRUE if DST is observed at any time during the current year.
-		foreach($this->transitions as $transition) {
-			if($transition['time'] >= $limit) {
+		for($i = 0, $transitionCount = count($this->transitions); $i < $transitionCount; ++$i) {
+			if($this->transitions[$i]['time'] >= $limit) {
 				break;
 			}
-			if($transition['time'] >= $start && $this->types[$transition['type']]['dstOffset'] != 0) {
+			if(($this->transitions[$i]['time'] >= $start && $this->types[$this->transitions[$i]['type']]['dstOffset'] != 0) || ($this->transitions[$i]['time'] > $start && $i > 0 && $this->types[$this->transitions[$i-1]['type']]['dstOffset'] != 0)) {
 				return true;
 			}
 		}
