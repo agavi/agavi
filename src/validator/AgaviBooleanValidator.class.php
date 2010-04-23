dd647de3d86eb394abb2c85139b4f36cb58672e9
David Zülke <david.zuelke@bitextender.com>
2010-04-23 13:39:31 +0000

    backport AgaviBooleanValidator from 194e12d40da90dc0812126f4f137553d338e9790...9f5783c768909ed1754b20063d9cd4da8c22d369 (from master), refs #1196

---
+++ src/validator/AgaviBooleanValidator.class.php
@@ -0,0 +1,86 @@
+<?php
+
+// +---------------------------------------------------------------------------+
+// | This file is part of the Agavi package.                                   |
+// | Copyright (c) 2005-2009 the Agavi Project.                                |
+// |                                                                           |
+// | For the full copyright and license information, please view the LICENSE   |
+// | file that was distributed with this source code. You can also view the    |
+// | LICENSE file online at http://www.agavi.org/LICENSE.txt                   |
+// |   vi: set noexpandtab:                                                    |
+// |   Local Variables:                                                        |
+// |   indent-tabs-mode: t                                                     |
+// |   End:                                                                    |
+// +---------------------------------------------------------------------------+
+
+/**
+ * AgaviBooleanValidator verifies a parameter is a valid boolean
+ * 
+ * Accepted values are string 0/1, int 0/1, bool true/false, string yes/no,
+ * string true/false, string on/off - basically all values that 
+ * {@see AgaviToolkit::literalize()} will accept.
+ * 
+ * The value will be casted to the respective boolean unless it's exported. If
+ * the export parameter is given, the value will be retained in its original
+ * form.
+ *
+ * @package    agavi
+ * @subpackage validator
+ *
+ * @author     Felix Gilcher <felix.gilcher@bitextender.com>
+ * @copyright  Authors
+ * @copyright  The Agavi Project
+ *
+ * @since      1.1.0
+ *
+ * @version    $Id$
+ */
+class AgaviBooleanValidator extends AgaviValidator
+{
+	/**
+	 * Validates the input.
+	 * 
+	 * @return     bool The value is a valid boolean
+	 * 
+	 * @author     Felix Gilcher <felix.gilcher@bitextender.com>
+	 * @since      1.1.0
+	 */
+	protected function validate()
+	{
+		$value = & $this->getData($this->getArgument());
+		$origValue = $value;
+		
+		if(is_bool($value)) {
+			// noop
+		} elseif(1 === $value || '1' === $value) {
+			$value = true;
+		} elseif(0 === $value || '0' === $value) {
+			$value = false;
+		} elseif(is_string($value)) {
+			$value = AgaviToolkit::literalize($value);
+		}
+		
+		if(is_bool($value)) {
+			
+			// we don't cast if the value is exported.
+			// caution, AgaviValidator::export does the test for empty
+			// strings, null and false values, so we can't use
+			// hasParameter here
+			if($this->getParameter('export')) {
+				$value = $origValue;
+			} else {
+				$this->export($value);
+			}
+			
+			return true;
+		}
+		
+		$value = $origValue;
+		
+		$this->throwError('type');
+		
+		return false;
+	}
+}
+
+?>
---
+++ test/tests/unit/validator/AgaviBooleanValidatorTest.php
@@ -0,0 +1,84 @@
+<?php
+
+class AgaviBooleanValidatorTest extends AgaviUnitTestCase
+{
+
+	/**
+	 * @var AgaviValidationManager
+	 */
+	protected $vm;
+	
+	public function setUp()
+	{
+		$this->vm = $this->getContext()->createInstanceFor('validation_manager');
+	}
+	
+	/**
+	 * @dataProvider validValues
+	 */
+	public function testAccept($value, $expectedResult)
+	{
+		$validator = $this->vm->createValidator('AgaviBooleanValidator', array('bool'), array('invalid argument'), array());
+		$rd = new AgaviRequestDataHolder(array(AgaviRequestDataHolder::SOURCE_PARAMETERS => array('bool' => $value)));
+		$result = $validator->execute($rd);
+		$this->assertEquals(AgaviValidator::SUCCESS, $result, 'Failed asserting that the validation succeeded.');
+		$this->assertEquals($expectedResult, $rd->getParameter('bool'), 'Failed asserting that the validated value is the expected value');
+	}
+
+	public function validValues() {
+		
+		return array(
+			'yes' => array('yes', true),
+			'no' => array('no', false),
+			'true' => array('true', true),
+			'false' => array('false', false),
+			'on' => array('on', true),
+			'off' => array('off', false),
+			'(bool)true' => array(true, true),
+			'(bool)false' => array(false, false),
+			'(int)1' => array(1, true),
+			'(int)0' => array(0, false),
+			'(string)1' => array('1', true),
+			'(string)0' => array('0', false)
+		);
+		
+	}
+	
+	/**
+	 * @dataProvider invalidValues
+	 */
+	public function testNotAccept($value)
+	{
+		$validator = $this->vm->createValidator('AgaviBooleanValidator', array('bool'), array('invalid argument'), array('export' => 'exported'));
+		$rd = new AgaviRequestDataHolder(array(AgaviRequestDataHolder::SOURCE_PARAMETERS => array('bool' => $value)));
+		$result = $validator->execute($rd);
+		$this->assertEquals(AgaviValidator::ERROR, $result, 'Failed asserting that the validation failed.');
+		$this->assertNull($rd->getParameter('exported'), 'Failed asserting that the value is not exported');
+		$this->assertEquals($value, $rd->getParameter('bool'), 'Failed asserting that the validated value is the original value');
+	}
+	
+	public function invalidValues() {
+		return array(
+			'nä' => array('nä'),
+			'nicht doch' => array('nicht doch'), 
+			'%core.debug%' => array('%core.debug%'), 
+			'foo' => array('foo'),
+			'(int)2' => array(2),
+			'(string)2' => array('2')
+		);
+	}
+	
+	public function testDontCastOnExport() {
+		$expectedResult = false;
+		$value = "false";
+		
+		$validator = $this->vm->createValidator('AgaviBooleanValidator', array('bool'), array('invalid argument'), array('export' => 'exported'));
+		$rd = new AgaviRequestDataHolder(array(AgaviRequestDataHolder::SOURCE_PARAMETERS => array('bool' => $value)));
+		$result = $validator->execute($rd);
+		$this->assertEquals(AgaviValidator::SUCCESS, $result, 'Failed asserting that the validation succeeded.');
+		$this->assertEquals($expectedResult, $rd->getParameter('exported'), 'Failed asserting that the exported value is the expected value');
+		$this->assertEquals($value, $rd->getParameter('bool'), 'Failed asserting that the validated value is the orginal value');
+	}
+}
+
+?>
