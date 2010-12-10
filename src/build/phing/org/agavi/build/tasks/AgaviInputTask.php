d5ba26f544725997bb01f2c97d811a7e581bf59c
David Zülke <david.zuelke@bitextender.com>
2010-12-10 22:36:44 +0000

    Fix #1333: Phing 2.4.4 causes fatal error

--- src/build/phing/org/agavi/build/tasks/AgaviInputTask.php
+++ src/build/phing/org/agavi/build/tasks/AgaviInputTask.php
@@ -14,6 +14,7 @@
 // +---------------------------------------------------------------------------+
 
 require_once(dirname(__FILE__) . '/AgaviTask.php');
+require_once('phing/input/InputRequest.php');
 
 /**
  * Requests an input value from the user.
