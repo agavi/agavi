ca3c6d8b8f2680f186aa50988e2800c34e76fdc6
David Zülke <david.zuelke@bitextender.com>
2010-12-19 01:44:58 +0000

    Refine #1283 to allow extraction of multiple subpatterns in regex validator, closes #1335 and closes #1283

--- src/validator/AgaviRegexValidator.class.php
+++ src/validator/AgaviRegexValidator.class.php
@@ -20,11 +20,14 @@
  * Parameters:
  *   'pattern'  PCRE to be used in preg_match
  *   'match'    input should match or not
- *   'extract'  Name of subpattern to extract and set or export on match
+ *   'export'   string with name of argument to export entire value to, or an
+ *              array of subpatterns names as keys and argument names as values
+ *              to selectively export one or more parts of the value
  * 
  * @package    agavi
  * @subpackage validator
  *
+ * @author     David Zülke <david.zuelke@bitextender.com>
  * @author     Dominik del Bondio <ddb@bitxtender.com>
  * @author     Uwe Mesecke <uwe@mesecke.net>
  * @copyright  Authors
@@ -41,12 +44,13 @@ class AgaviRegexValidator extends AgaviValidator
 	 * 
 	 * @return     bool True if input matches the pattern in 'match'.
 	 * 
+	 * @author     David Zülke <david.zuelke@bitextender.com>
 	 * @author     Uwe Mesecke <uwe@mesecke.net>
 	 * @since      0.11.0
 	 */
 	protected function validate()
 	{
-		$value = $data =& $this->getData($this->getArgument());
+		$data = $this->getData($this->getArgument());
 		if(!is_scalar($data)) {
 			// non scalar values would cause notices
 			$this->throwError();
@@ -60,14 +64,20 @@ class AgaviRegexValidator extends AgaviValidator
 			return false;
 		}
 		
-		if($result && ($extract = $this->getParameter('extract')) !== null && isset($matches[$extract])) {
-			$value = $matches[$extract];
-		}
-		
 		if($this->hasParameter('export')) {
-			$this->export($value);
-		} else {
-			$data = $value;
+			$export = $this->getParameter('export');
+			// if the result was positive (makes no sense for negative matches) and "export" is an array...
+			if($result && is_array($export)) {
+				// ...treat it as a map of subpattern names and argument names for exporting parts of the value
+				foreach($export as $subpattern => $argument) {
+					if(isset($matches[$subpattern])) {
+						$this->export($matches[$subpattern], $argument);
+					}
+				}
+			} else {
+				// otherwise, just export the whole input
+				$this->export($data);
+			}
 		}
 		
 		return true;
