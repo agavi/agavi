d1c876a5702ec281d3abcd44f69ed77d773f59dd
David Zülke <david.zuelke@bitextender.com>
2011-04-20 15:58:01 +0000

    make sure core.config_dir is set for tests, this got broken in dbd3b53bd194f854c84842b35484f4ebc18c8d7d, refs #380 and #1357

--- test/config.php
+++ test/config.php
@@ -2,6 +2,6 @@
 
 AgaviConfig::set('core.testing_dir', realpath(dirname(__FILE__)));
 AgaviConfig::set('core.app_dir', realpath(dirname(__FILE__).'/sandbox/app/'));
-
+AgaviConfig::set('core.cache_dir', AgaviConfig::get('core.app_dir') . '/cache'); // for the clearCache() before bootstrap()
 
 ?>
