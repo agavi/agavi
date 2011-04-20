f7b9a60ef18c532b3a6fabd0aa42bc00a06656ee
David Zülke <david.zuelke@bitextender.com>
2011-04-20 10:11:00 +0000

    Change AgaviSecurityUser::hasCredentials() to use AgaviSecurityUser::hasCredential() for complex lookups and refactor/simplify method, closes #1356

--- src/user/AgaviSecurityUser.class.php
+++ src/user/AgaviSecurityUser.class.php
@@ -95,28 +95,25 @@ class AgaviSecurityUser extends AgaviUser implements AgaviISecurityUser
 	 * @author     David Zülke <dz@bitxtender.com>
 	 * @since      0.9.0
 	 */
-	public function hasCredentials($credential)
+	public function hasCredentials($credentials)
 	{
-		if(is_array($credential)) {
-			$credentials = (array)$credential;
-			foreach($credentials as $credential) {
-				if(is_array($credential)) {
-					foreach($credential as $subcred) {
-						if(in_array($subcred, $this->credentials, true)) {
-							continue 2;
-						}
-					}
-					return false;
-				} else {
-					if(!in_array($credential, $this->credentials, true)) {
-						return false;
+		foreach((array)$credentials as $credential) {
+			if(is_array($credential)) {
+				// OR
+				foreach($credential as $subcred) {
+					if($this->hasCredential($subcred)) {
+						continue 2;
 					}
 				}
+				return false;
+			} else {
+				// AND
+				if(!$this->hasCredential($credential)) {
+					return false;
+				}
 			}
-			return true;
-		} else {
-			return $this->hasCredential($credential);
 		}
+		return true;
 	}
 	
 	/**
