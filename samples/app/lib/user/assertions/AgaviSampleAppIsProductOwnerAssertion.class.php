f5eba222dfcce7814419669ad9f86947d36bb3cf
David Zülke <david.zuelke@bitextender.com>
2011-06-25 03:54:35 +0000

    assertion class forgotten in 4980c940cccbbb5e2214da9fb8eb2cd439268a83, refs #1394

---
+++ samples/app/lib/user/assertions/AgaviSampleAppIsProductOwnerAssertion.class.php
@@ -0,0 +1,23 @@
+<?php
+
+class AgaviSampleAppIsProductOwnerAssertion implements Zend_Acl_Assert_Interface
+{
+	public function assert(Zend_Acl $acl, Zend_Acl_Role_Interface $role = null, Zend_Acl_Resource_Interface $resource = null, $privilege = null)
+	{
+		if(!($resource instanceof ProductModel)) {
+			// in case the check is performed without a specific product instance:
+			// let's assume that the user can edit a generic product
+			return true;
+		}
+		
+		if(!($role instanceof AgaviUser)) {
+			// in case the check is performed without a specific product instance:
+			// let's assume that any generic user cannot edit this product
+			return false;
+		}
+		
+		return $resource->getOwner() == $role->getAttribute('username');
+	}
+}
+
+?>
