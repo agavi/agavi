0f994d1ec43632d906b489416caeff6bb2f6c693
David Zülke <david.zuelke@bitextender.com>
2011-12-11 02:16:27 +0000

    Update singular and plural rules in AgaviInflector and fix an error where it does not handle uncountable words as part of strings. Also adds a boatload of tests. Closes #1460 and closes #1459

--- src/util/AgaviInflector.class.php
+++ src/util/AgaviInflector.class.php
@@ -31,28 +31,61 @@
 final class AgaviInflector
 {
 	/**
+	 * @const      string
+	 */
+		const UNCOUNTABLE_REGEX = '/(
+			advice|
+			equipment|
+			information|
+			(?<![a-z0-9])rice| # after a boundary, or else "price" is treated the same; cannot use \b because that includes the underscore
+			sugar|
+			water|
+			electricity|
+			gas|
+			power|
+			money|
+			music|
+			love|
+			furniture|
+			(lug|bag)gage|
+			species|
+			series|
+			bison|
+			deer|
+			fish|
+			moose|
+			sheep|
+			jeans
+		)$/ix';
+	
+	/**
 	 * @var        array singular => plural mapping
 	 */
 	protected static $singularMatches = array(
-		'/move$/i' => 'moves',
-		'/sex$/i' => 'sexes',
+		self::UNCOUNTABLE_REGEX => '$1',
 		'/child$/i' => 'children',
 		'/man$/i' => 'men',
 		'/person$/i' => 'people',
 		'/(quiz)$/i' => '$1zes',
-		'/^(ox)$/i' => '$1en',
+		'/(?<![a-z0-9])(ox)(en)?$/i' => '$1en',
+		'/(?<![a-z0-9])tooth$/i' => 'teeth', // cannot use \b because that includes the underscore
+		'/(?<![a-z0-9])foot$/i' => 'feet', // cannot use \b because that includes the underscore
+		'/(?<![a-z0-9])goose$/i' => 'geese', // cannot use \b because that includes the underscore
+		'/(m|l)ice$/i' => '$1ice',
 		'/(m|l)ouse$/i' => '$1ice',
-		'/(matr|vert|ind)ix|ex$/i' => '$1ices',
+		'/(matr|vert|ind)(?:ix|ex)$/i' => '$1ices',
 		'/(x|ch|ss|sh)$/i' => '$1es',
-		'/([^aeiouy]|qu)ies$/i' => '$1y',
 		'/([^aeiouy]|qu)y$/i' => '$1ies',
 		'/(hive)$/i' => '$1s',
 		'/(?:([^f])fe|([lr])f)$/i' => '$1$2ves',
 		'/sis$/i' => 'ses',
+		'/criterion$/i' => 'criteria',
+		'/([ti])a$/i' => '$1a',
 		'/([ti])um$/i' => '$1a',
 		'/(buffal|tomat)o$/i' => '$1oes',
 		'/(bu)s$/i' => '$1ses',
 		'/(alias|status)$/i' => '$1es',
+		'/(octop|vir)i$/i' => '$1i',
 		'/(octop|vir)us$/i' => '$1i',
 		'/(ax|test)is$/i' => '$1es',
 		'/s$/i' => 's',
@@ -63,27 +96,27 @@ final class AgaviInflector
 	 * @var        array plural => singular mapping
 	 */
 	protected static $pluralMatches = array(
-		'/cookies$/i' => 'cookie',
-		'/moves$/i' => 'move',
-		'/sexes$/i' => 'sex',
+		self::UNCOUNTABLE_REGEX => '\1',
 		'/children$/i' => 'child',
 		'/men$/i' => 'man',
 		'/people$/i' => 'person',
-		'/databases$/i'=> 'database',
-		'/caches$/i'=> 'cache',
+		'/(database)s$/i' => '\1',
 		'/(quiz)zes$/i' => '\1',
 		'/(matr)ices$/i' => '\1ix',
 		'/(vert|ind)ices$/i' => '\1ex',
-		'/^(ox)en/i' => '\1',
+		'/(?<![a-z0-9])(ox)en/i' => '\1',
 		'/(alias|status)es$/i' => '\1',
-		'/([octop|vir])i$/i' => '\1us',
+		'/(octop|vir)i$/i' => '\1us',
 		'/(cris|ax|test)es$/i' => '\1is',
-		'/(shoe)s$/i' => '\1',
+		'/(cook|zomb|mov)ies$/i' => '\1ie',
+		'/(sho|cach|mov)es$/i' => '\1e',
 		'/(o)es$/i' => '\1',
 		'/(bus)es$/i' => '\1',
-		'/([m|l])ice$/i' => '\1ouse',
+		'/teeth$/i' => 'tooth',
+		'/feet$/i' => 'foot',
+		'/geese$/i' => 'goose',
+		'/(m|l)ice$/i' => '\1ouse',
 		'/(x|ch|ss|sh)es$/i' => '\1',
-		'/(m)ovies$/i' => '\1ovie',
 		'/(s)eries$/i' => '\1eries',
 		'/([^aeiouy]|qu)ies$/i' => '\1y',
 		'/([lr])ves$/i' => '\1f',
@@ -92,25 +125,12 @@ final class AgaviInflector
 		'/([^f])ves$/i' => '\1fe',
 		'/(^analy)ses$/i' => '\1sis',
 		'/((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$/i' => '\1\2sis',
+		'/criteria$/i' => 'criterion',
 		'/([ti])a$/i' => '\1um',
 		'/(n)ews$/i' => '\1ews',
 		'/s$/i' => '',
 	);
 
-	/**
-	 * @var        array An array of uncountable nouns as keys
-	 */
-	protected static $uncountables = array(
-		'equipment' => true,
-		'information' => true,
-		'rice' => true,
-		'money' => true,
-		'species' => true,
-		'series' => true,
-		'fish' => true,
-		'sheep' => true,
-	);
-	
 	/**
 	 * @var        array An array remembering the results of singularize()
 	 */
@@ -138,10 +158,6 @@ final class AgaviInflector
 			return self::$singularizeCache[$word];
 		}
 		
-		if(isset(self::$uncountables[$word])) {
-			return $word;
-		}
-
 		$count = 0;
 		$singularizedWord = $word;
 		foreach(self::$pluralMatches as $regexp => $replacement) {
@@ -173,10 +189,6 @@ final class AgaviInflector
 			return self::$pluralizeCache[$word];
 		}
 		
-		if(isset(self::$uncountables[$word])) {
-			return $word;
-		}
-
 		$count = 0;
 		$pluralizedWord = $word;
 		foreach(self::$singularMatches as $regexp => $replacement) {
