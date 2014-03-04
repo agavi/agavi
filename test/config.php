b47b8f4fcda2037613b45e85e7b56e63d2f14347
Leon Weidauer <leon.weidauer@googlemail.com>
2014-03-04 20:13:22 +0100

    Added timezone fallback to tests config

--- test/config.php
+++ test/config.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * If PHP has no timezone set, use London (UTC) as a fallback.
+ * Otherwise the tests would fail unnecessarily.
+ */
+if (!ini_get('date.timezone')) {
+    ini_set('date.timezone', 'Europe/London');
+}
+
 AgaviConfig::set('core.testing_dir', realpath(__DIR__));
 AgaviConfig::set('core.app_dir', realpath(__DIR__.'/sandbox/app/'));
 AgaviConfig::set('core.cache_dir', AgaviConfig::get('core.app_dir') . '/cache'); // for the clearCache() before bootstrap()
