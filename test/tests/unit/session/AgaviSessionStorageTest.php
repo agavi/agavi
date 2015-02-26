4a60ea10585b01443aad4ff22cfc289e199ab5de
Dominik del Bondio <dominik.del.bondio@bitextender.com>
2015-02-26 18:11:01 +0100

    revert (code) changes from b9afe59 The code was actually working as intended and no change necessary

--- test/tests/unit/session/AgaviSessionStorageTest.php
+++ test/tests/unit/session/AgaviSessionStorageTest.php
@@ -3,29 +3,16 @@
 class AgaviSessionStorageTest extends AgaviUnitTestCase
 {
 	
-	/**
-	 * @dataProvider dataStartupSetsCookieSecureFlag
-	 */
 	public function testStartupSetsCookieSecureFlag($iniValue, $secure)
 	{
-		ini_set('session.cookie_secure', $iniValue);
-		$context = AgaviContext::getInstance('agavi-session-storage-test::tests-startup-sets-cookie-secure-flag');
 		// test for bug #1541
+		ini_set('session.cookie_secure', 0);
+		$context = AgaviContext::getInstance('agavi-session-storage-test::tests-startup-sets-cookie-secure-flag');
 		$storage = new AgaviSessionStorage();
 		$storage->initialize($context);
 		$storage->startup();
 		$cookieParams = session_get_cookie_params();
-		$this->assertSame($secure, $cookieParams['secure']);
-	}
-	
-	public function dataStartupSetsCookieSecureFlag()
-	{
-		return array(
-			array(null, true),
-			array('', true),
-			array('off', false),
-			array('on', true),
-		);
+		$this->assertTrue($cookieParams['secure']);
 	}
 	
 }
